121bf96c611e3c1b4e4268bea07c85008e27a347
Evan Czaplicki <info@elm-lang.org>
2012-06-07 21:45:23 -0500

    Simplify and add instructive comments.

--- examples/Intermediate/SlideShow.elm
+++ examples/Intermediate/SlideShow.elm
@@ -1,6 +1,4 @@
 
--- Creating the slide-show
-
 ith i lst = case lst of { x:xs -> if i == 0 then x else ith (i-1) xs }
 
 images = [ "book.jpg", "shells.jpg", "stack.jpg", "car.jpg", "pipe.jpg" ]
@@ -9,12 +7,12 @@ slideShow (w,h) index =
   size w h . color black . box 5 . image $ ith i images
 
 
--- Two ways to run the slide-show 
-
 clickCount = foldp (\b c -> if b then c + 1 else c) 0 Mouse.isClicked
 tickCount t = foldp (\x c -> c + 1) 0 (Time.every t)
 
 
--- Putting it all together
+-- Be sure to also try this with `clickCount` which switches images
+-- each time the user clicks the mouse. You can also try changing the
+-- time interval (i.e. `tickCount 7`)
 
 main = lift2 slideShow Window.dimensions (tickCount 4)
