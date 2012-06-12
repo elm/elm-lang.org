090858eb99edff6d399dc4b5714f9769cee2e0fd
Evan Czaplicki <info@elm-lang.org>
2012-06-12 04:41:32 -0500

    Update docs to depend on the Website.Docs module which has all of the display code!

--- docs/Data/List.elm
+++ docs/Data/List.elm
@@ -1,47 +1,8 @@
 
-button (name, href) = size 100 60 . Element.link href . size 100 60 . box 5 $ plainText name
-buttons = size 400 60 . flow right . List.map button $
-  [ ("Home","/"), ("Examples","/Examples.elm"), ("Docs","/Documentation.elm"), ("Download","/Download.elm") ]
-
-title w = size w 60 . box 4 . text . header . toText $ "Elm"
-
-lightGrey = rgb (245/255) (245/255) (245/255)
-mediumGrey = rgb (216/255) (221/255) (225/255)
-heading outer inner =
-  color mediumGrey . size outer 61 . box 1 .
-  color  lightGrey . size outer 60 . box 5 .
-  size inner 60 . box 5 $ title (inner-400) `beside` buttons
-
-skeleton body outer =
-  let inner = if outer < 820 then outer - 20 else 800 in
-  flow down [ heading outer inner
-            , width outer . box 2 $ plainText "&nbsp;" `above` body inner
-            , size outer 50 . box 8 . text . Text.color mediumGrey $
-                toText "&copy; 2011-2012 Evan Czaplicki" 
-            ]
-
-addSpaces px = List.map (\f -> f ()) . List.intersperse (\x -> height px $ plainText "") . List.map (\e x -> e)
-
-----------------------
-
-section = text . bold . Text.height (5/4) . toText
-
-darkerGrey = rgb (114/255) (124/255) (129/255)
-entry w (name, type, desc) =
-  flow down . List.map (width w) $
-    [ text . monospace . toText $ name ++ " :: " ++ type
-    , text . Text.color darkerGrey . toText $ "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" ++ desc ]
-
-group w (name, fs) = flow down . addSpaces 5 . List.map (width w) $ (text . bold $ toText name) : List.map (entry w) fs
-
-createDocs name cats =
-  let f w = flow down . addSpaces 30 $ section name : List.map (group w) cats in
-  lift (skeleton f) Window.width
-
-----------------------
+import Website.Docs (createDocs)
 
 basics =
-  [ ("(:)", "a -> [a] -> [a]", "Add an element to the front of a list. Thus, (a : [b,c] = [a,b,c])")
+  [ ("(:)", "a -> [a] -> [a]", "Add an element to the front of a list (a : [b,c] = [a,b,c]).")
   , ("(++)", "[a] -> [a] -> [a]", "Appends two lists.")
   , ("head", "[a] -> a", "Extract the first element of a list. List must be non-empty.")
   , ("tail", "[a] -> [a]", "Extract the elements after the head of the list. List must be non-empty.")
@@ -92,4 +53,4 @@ categories =
   , ("Special Folds", special)
   , ("Other Useful Functions", other) ]
 
-main = createDocs "List" categories
+main = createDocs "Data.List" categories
--- docs/Line.elm
+++ docs/Line.elm
@@ -1,45 +1,5 @@
 
-button (name, href) = size 100 60 . Element.link href . size 100 60 . box 5 $ plainText name
-buttons = size 400 60 . flow right . List.map button $
-  [ ("Home","/"), ("Examples","/Examples.elm"), ("Docs","/Documentation.elm"), ("Download","/Download.elm") ]
-
-title w = size w 60 . box 4 . text . header . toText $ "Elm"
-
-lightGrey = rgb (245/255) (245/255) (245/255)
-mediumGrey = rgb (216/255) (221/255) (225/255)
-heading outer inner =
-  color mediumGrey . size outer 61 . box 1 .
-  color  lightGrey . size outer 60 . box 5 .
-  size inner 60 . box 5 $ title (inner-400) `beside` buttons
-
-skeleton body outer =
-  let inner = if outer < 820 then outer - 20 else 800 in
-  flow down [ heading outer inner
-            , width outer . box 2 $ plainText "&nbsp;" `above` body inner
-            , size outer 50 . box 8 . text . Text.color mediumGrey $
-                toText "&copy; 2011-2012 Evan Czaplicki" 
-            ]
-
-addSpaces px = List.map (\f -> f ()) . List.intersperse (\x -> height px $ plainText "") . List.map (\e x -> e)
-
-----------------------
-
-section = text . bold . Text.height (5/4) . toText
-
-darkerGrey = rgb (114/255) (124/255) (129/255)
-entry w (name, type, desc) =
-  flow down . List.map (width w) $
-    [ text . monospace . toText $ name ++ " :: " ++ type
-    , text . Text.color darkerGrey . toText $ "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" ++ desc ]
-
-group w (name, fs) = flow down . addSpaces 5 . List.map (width w) $ (text . bold $ toText name) : List.map (entry w) fs
-
-createDocs name cats =
-  let f w = flow down . addSpaces 30 $ section name : List.map (group w) cats in
-  lift (skeleton f) Window.width
-
-----------------------
-
+import Website.Docs (createDocs)
 
 creation =
   [ ("line", "[(Number,Number)] -> Line", "Create an arbitrary line from an list of vertexes.")
