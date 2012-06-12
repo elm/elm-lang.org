5e6ea4f4a81cc2c9b442a6f7e3749e38db22d2d6
Evan Czaplicki <info@elm-lang.org>
2012-06-12 05:39:37 -0500

    Improve the keyboard examples a little.

--- examples/Reactive/CharPressed.elm
+++ examples/Reactive/CharPressed.elm
@@ -2,7 +2,13 @@
 -- Focus on the display screen (i.e. click the right half of this window)
 -- and start pressing keys!
 
-update chr oldChr = case chr of { Just c -> c ; Nothing -> oldChr }
-latest sig = foldp update '_' sig
+import Data.Char (fromCode)
 
-main = lift asText $ latest Keyboard.Raw.charPressed
+update chr oldChr =
+  case chr of { Just c -> fromCode c ; Nothing -> oldChr }
+latestKey = foldp update '_' Keyboard.Raw.charPressed 
+
+display chr =
+  text $ toText "The last key you pressed was: " ++ show chr
+
+main = lift display latestKey
--- examples/Reactive/KeysDown.elm
+++ examples/Reactive/KeysDown.elm
@@ -2,4 +2,7 @@
 -- Focus on the display screen (i.e. click the right half of this window)
 -- and start pressing keys!
 
-main = lift asText Keyboard.Raw.keysDown
+display codes =
+  text $ toText "You are holding down the following keys: " ++ show codes
+
+main = lift display Keyboard.Raw.keysDown
