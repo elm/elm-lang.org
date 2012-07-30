4f466db310537fdb12a516237921dc4361e42481
Evan Czaplicki <info@elm-lang.org>
2012-07-30 13:56:38 +0200

    Fix error in docs.

--- docs/Shape.elm
+++ docs/Shape.elm
@@ -16,7 +16,7 @@ toForm =
 
 transform =
   [ ("move", "Number -> Number -> Shape -> Shape", "Translate a shape by a given x and y offset.")
-  , ("rotate", "Number -> Number -> Shape -> Shape", "Rotate a shape by a given fraction of a full turn (not degrees or radians). For instance, (rotate (1/2) s) will rotate s by a half turn.")
+  , ("rotate", "Number -> Shape -> Shape", "Rotate a shape by a given fraction of a full turn (not degrees or radians). For instance, (rotate (1/2) s) will rotate s by a half turn.")
   , ("scale", "Number -> Shape -> Shape", "Scale a shape by a given scale factor.")
   ]
 
