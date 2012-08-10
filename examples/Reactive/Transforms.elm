02d78e09a954ed351bf54c05ec5b03a4c1a2fd91
Evan Czaplicki <info@elm-lang.org>
2012-08-10 21:58:16 +0200

    Fix type error!

--- examples/Reactive/Transforms.elm
+++ examples/Reactive/Transforms.elm
@@ -8,7 +8,7 @@ myBlue  = rgb 0 85 170
 myGreen = rgba 28 267 85 (1/2)
 
 scene (x,y) (w,h) =
-  collage w h [ filled myBlue . rotate ((x+y)/1000) $ ngon 4 100 (200,200)
+  collage w h [ filled myBlue . rotate (toFloat (x+y) / 1000) $ ngon 4 100 (200,200)
               , filled myGreen $ ngon 5 30 (x,y)
               ]
 
