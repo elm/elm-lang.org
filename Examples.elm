3e07a6c76c1de5279f74b991275f7477f468b063
Evan Czaplicki <info@elm-lang.org>
2012-08-21 15:20:35 +0200

    Add link to games blog.

--- Examples.elm
+++ Examples.elm
@@ -30,6 +30,8 @@ content w =
            toText " &#8212; serve Elm code without leaving Haskell"
   , text $ link "/examples/ThisWebsite.elm" (toText "This Website") ++
            toText " &#8212; the Elm source code for this site"
+  , text $ link "/blog/games-in-elm/part-0/Making-Pong.html" (toText "Elm for Games") ++
+           toText " &#8212; how to make games in Elm"
   , rectangle 1 5
   , section "Quick Overview of Elm"
   , plainText "If you just want a brief overview, check out the following examples:"
