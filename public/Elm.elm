649aaffdc509d3ea852cb6ed7c71b7877fa613a5
Evan Czaplicki <info@elm-lang.org>
2012-10-09 21:56:33 -0700

    Update Elm home page.

--- public/Elm.elm
+++ public/Elm.elm
@@ -5,52 +5,49 @@ intro = [markdown|
 
 ### The Elm Programming Language
 
-The Elm programming language aims to make web development
-more pleasant. Elm is a type-safe, functional reactive language
-that compiles to HTML, CSS, and JavaScript. You can start coding
-in Elm without any install or setup with Elm's [interactive editor][1],
-so start learning Elm by [example][2]:
+The Elm programming language aims to make web development more pleasant.
+There are tons of [interactive examples][examples], so it easy to learn
+and use Elm for beginners and experts alike.
 
-  [1]: /edit/examples/Reactive/Transforms.elm "interactive editor"
-  [2]: /Examples.elm "example"
+  [examples]: /Examples.elm "example"
 
-|]
+Elm compiles to HTML, CSS and JavaScript. So you can use the [web platform][web]
+with all the benefits of a modern programming language.
+Elm's features include:
 
-exampleData =
-  [ ("Layout"       , "FlowDown2" , "Elements/")
-  , ("Shapes"       , "Shapes"    , "Elements/")
-  , ("Analog Clock" , "Clock"     , "Intermediate/")
-  , ("Slide Show"   , "SlideShow" , "Intermediate/")
-  , ("Form Validation", "Form"    , "Intermediate/")
-  ]
+- [Strongly][strong] / [statically][static] typed with [type inference][infer]
+- [Functional Reactive Programming][frp] (FRP)
+- [Markdown support][md]
+- [Module system][module]
+- [Growing set of core libraries](/Documentation.elm)
+- [Online interactive editor][ide]
+- [Purely functional][pure] graphics
+- [Pattern matching][match]
+- [Call-by-Value evaluation][cbv]
 
-examples w = let tiles = tile w $ map toTile exampleData in
-             container w (heightOf tiles) middle tiles
 
-rest = [markdown|
+  [strong]: http://en.wikipedia.org/wiki/Strong_typing "strongly"
+  [static]: http://en.wikipedia.org/wiki/Type_system#Static_typing "statically"
+  [infer]:  http://en.wikipedia.org/wiki/Type_inference "type inference"
+  [frp]:    http://en.wikipedia.org/wiki/Functional_reactive_programming "functional reactive programming"
+  [module]: http://en.wikipedia.org/wiki/Modular_programming "modules"
+  [md]:     http://daringfireball.net/projects/markdown/basics "markdown"
+  [ide]:    /edit/examples/Reactive/Transforms.elm "interactive editor"
+  [pure]:   http://en.wikipedia.org/wiki/Purely_functional "purely functional"
+  [match]:  http://en.wikipedia.org/wiki/Pattern_matching "pattern matching"
+  [cbv]:    http://en.wikipedia.org/wiki/Evaluation_strategy "call-by-value"
 
-Elm's major distinguishing features are support for [reactive programming][1]
-and its focus on graphical user interfaces. Elm is also [call-by-value][2] and
-[strongly][3] / [statically][4] typed with [type inference][5], so those of you
-already familiar with languages like Haskell or ML should be quite comfortable.
 
-The language features mentioned above help catch errors in your code at
-compile time, but there are some errors that are external to your code,
-such as browser incompatabilities. By using HTML, CSS, and JavaScript as
-an assembly language, Elm can avoid some of these problems.
+  [web]: http://www.webplatform.org/ "web platform"
 
-  [1]: http://en.wikipedia.org/wiki/Reactive_programming "reactive programming"
-  [2]: http://en.wikipedia.org/wiki/Evaluation_strategy "call-by-value"
-  [3]: http://en.wikipedia.org/wiki/Strong_typing "strongly"
-  [4]: http://en.wikipedia.org/wiki/Type_system#Static_typing "statically"
-  [5]: http://en.wikipedia.org/wiki/Type_inference "type inference"
+For a quick introduction to Elm, definitely look at the [basic examples](/examples/Basic.elm).
 
 <br/>
 
 #### News
 
 Elm 0.4: New features for game making and layout, Markdown support,
-optimizations, and more. More information [here][6].
+optimizations, and more. More info [here][6].
 
   [6]: /blog/announce/version-0.4.0.elm "announcement"
 
@@ -62,6 +59,6 @@ See the Elm [mailing list][7] for questions, announcements, and discussion. You 
 
 |]
 
-info w = flow down $ map (width w) [ intro, examples w, rest ]
+info w = flow down $ map (width w) [ intro ]
 
 main = lift (skeleton info) Window.width
