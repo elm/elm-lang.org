1e7b5c0624e9490fc98c76a07c621f070d70f961
Evan Czaplicki <info@elm-lang.org>
2012-10-23 01:15:35 -0700

    Add info for `complement` function.

--- public/docs/Graphics/Color.elm
+++ public/docs/Graphics/Color.elm
@@ -4,6 +4,7 @@ import Website.Docs (createDocs)
 creation =
     [ ("rgb", "Int -> Int -> Int -> Color", "Create rgb colors from numbers between 0 and 255 inclusive.")
     , ("rgba", "Int -> Int -> Int -> Float -> Color", "Create colors with an alpha component for transparency. The color is specified with numbers between 0 and 255 inclusive, whereas the alpha component is specified with numbers between 0 and 1.")
+    , ("complement", "Color -> Color", "Produces a &ldquo;complementary color&rdquo;. The two colors will accent each other.")
     ]
 
 builtin =
