bfa35e7b5d19a4199ae39d72e5d751b8b9ea06cf
Benjamin R. Haskell <elm@benizi.com>
2012-12-09 13:03:11 -0500

    Tweaks the compile script to prevent error message

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 cd server
-ghc --make -O2 -threaded -hidir ghc_output -odir ghc_output Server.hs
-mv Server ..
+ghc --make -O2 -threaded -hidir ghc_output -odir ghc_output Server.hs \
+&& mv Server ..
 cd ..
