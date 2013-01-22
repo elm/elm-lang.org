136ef9e7edc2cda7129c95d8b9f2ac96c6a1a47e
Evan Czaplicki <info@elm-lang.org>
2013-01-21 16:12:34 -0800

    Add comments about how to get the touch examples working and add links to them from /Basic.elm.

--- public/examples/Reactive/Draw.elm
+++ public/examples/Reactive/Draw.elm
@@ -1,4 +1,7 @@
 
+-- Try this out on an iOS or Android device. For best results
+-- use the "In Tab" compile option.
+
 add t d = let vs = Dict.findWithDefault [] t.id d
           in  Dict.insert t.id ((t.x,t.y):vs) d
 
--- public/examples/Reactive/Touch.elm
+++ public/examples/Reactive/Touch.elm
@@ -1,10 +1,15 @@
 
-scene (w,h) = collage w h . map (\{x,y} -> outlined green $ circle 60 (x,y))
+-- Try this out on an iOS or Android device. For best results
+-- use the "In Tab" compile option.
 
+scene (w,h) = collage w h . map (\{x,y} -> outlined green $ circle 60 (x,y))
 main = lift2 scene Window.dimensions Touch.touches
 
 
-content = constant (JavaScript.castStringToJSString
-                    "width=device-width, initial-scale=1")
+-- Force mobile devices to accurately report their dimensions:
+
 foreign export jsevent "elm_viewport"
   content :: Signal JSString
+
+content = let c = "width=device-width, initial-scale=1"
+          in  constant (JavaScript.castStringToJSString c)
