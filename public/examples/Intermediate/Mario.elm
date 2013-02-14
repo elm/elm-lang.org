13aff238785064557192e788e9c6ac5bd7145ce6
Evan Czaplicki <info@elm-lang.org>
2013-02-13 17:14:26 -0800

    Clean up the code for Mario.

--- public/examples/Intermediate/Mario.elm
+++ public/examples/Intermediate/Mario.elm
@@ -1,30 +1,34 @@
 
-module Mario where
-
+-- Model
 mario = { x = 0, y = 0, vx = 0, vy = 0, dir = "right" }
 
-jumpStep isJump obj = if isJump && obj.y == 0 then { obj | vy <- 5 } else obj
+-- Update for Keyboard Input
+jumpStep {y} obj = if y > 0 && obj.y == 0 then { obj | vy <- 5 } else obj
+walkStep {x} obj = { obj | vx <- x, dir <- if | x < 0     -> "left"
+                                              | x > 0     -> "right"
+                                              | otherwise -> obj.dir }
+
+-- Update for Time Input
 gravityStep t obj = { obj | vy <- if obj.y > 0 then obj.vy - t/4 else obj.vy }
 timeStep t obj = let {x,y,vx,vy} = obj in
-                 { obj | x <- x + t * vx , y <- max 0 $ y + t * vy }
-walkStep dir obj = { obj | vx <- dir, dir <- if | dir < 0   -> "left"
-                                                | dir > 0   -> "right"
-                                                | otherwise -> obj.dir }
+                 { obj | x <- x + t*vx , y <- max 0 (y + t*vy) }
 
-step t d j = timeStep t . gravityStep t . jumpStep j . walkStep d
+-- Update
+step (t,dir) = timeStep t . gravityStep t . jumpStep dir . walkStep dir
 
+-- Inputs
+input = let delta = lift (\t -> t/20) (fps 25)
+        in sampleOn delta (lift2 (,) delta Keyboard.arrows)
 
-delta = lift (flip (/) 20) (fps 25)
-leftRight = toFloat . .x <~ Keyboard.arrows
-jump = (\{y} -> y > 0) <~ Keyboard.arrows
-steps = sampleOn delta (lift3 step delta leftRight jump)
-
-main  = lift2 render Window.dimensions (foldp ($) mario steps)
-
+-- Display
 render (w,h) mario =
   let verb = if mario.y  >  0 then "jump" else
              if mario.vx /= 0 then "walk" else "stand"
       src  = "/imgs/mario/" ++ verb ++ "/" ++ mario.dir ++ ".gif"
-  in  collage w h [ filled (rgb 174 238 238) $ rect w h (w `div` 2, h `div` 2)
-                  , filled (rgb 74 163 41) $ rect w 50 (w `div` 2,h-25)
-                  , toForm (mario.x, (h-63)-mario.y) (image 35 35 src) ]
+  in collage w h
+       [ filled (rgb 174 238 238) $ rect w h (w `div` 2, h `div` 2)
+       , filled (rgb 74 163 41) $ rect w 50 (w `div` 2,h-25)
+       , toForm (mario.x, (h-63)-mario.y) (image 35 35 src) ]
+
+-- Putting it all together
+main  = lift2 render Window.dimensions (foldp step mario input)
