532033fb450819540b3d6800cb0e3813a3b4fd27
Evan Czaplicki <info@elm-lang.org>
2013-05-25 15:32:10 +0200

    Fancy up the sliding ball and make Web.elm simpler.

--- public/examples/Intermediate/Slide.elm
+++ public/examples/Intermediate/Slide.elm
@@ -3,23 +3,29 @@ import Either
 import Mouse
 import Window
 
+-- MODEL
 data Update = Click (Int,Int) | TimeDelta Time
 
 input = let clickPos = sampleOn Mouse.clicks Mouse.position
         in  merge (Click <~ clickPos)
                   (TimeDelta <~ (40 `fpsWhen` (second `since` clickPos)))
 
+-- UPDATE
 step inp ((tx,ty),(x,y)) =
     case inp of
       Click t  -> (t, (x,y))
       TimeDelta d -> ((tx,ty), ( x + (tx-x) * (d/100) ,
                                  y + (ty-y) * (d/100) ))
 
+-- DISPLAY
+greenGrad = radial (0,0) 10 (7,0-5) 30
+              [(0, rgb 167 211 12), (0.9, rgb 1 159 98), (1, rgba 1 159 98 0)]
+
 follower (w,h) (target,(x,y)) =
-  layers [ collage w h [ circle 16 |> filled cyan
-                                   |> move (x- toFloat w/2, toFloat h/2-y) ]
+  layers [ collage w h [ circle 100 |> gradient greenGrad
+                                    |> move (x- toFloat w/2, toFloat h/2-y) ]
          , plainText "Click anywhere and the circle will follow." ]
 
-main = lift2 follower Window.dimensions
-                      (foldp step ((0,0),(0,0)) input)
+main = follower <~ Window.dimensions
+                 ~ foldp step ((0,0),(0,0)) input
 
--- public/examples/Intermediate/Web.elm
+++ public/examples/Intermediate/Web.elm
@@ -1,11 +1,10 @@
 
 quadrant spc n =
-    let scale = map ((*) spc)
-        xs = map (\x -> (x,0)) . scale <| [0..n]
-        ys = map (\y -> (0,y)) . scale <| reverse [0..n]
+    let xs = map (\x -> (x * spc, 0)) <| [0..n]
+        ys = map (\y -> (0, y * spc)) <| reverse [0..n]
     in  map (traced (solid black)) (zipWith segment xs ys)
 
 quad angle =
-    rotate (degrees angle) . group <| quadrant 5 30
+    rotate (degrees angle) . group <| quadrant 8 20
 
 main = collage 300 300 <| map quad [ 0, 90, 180, 270 ]
