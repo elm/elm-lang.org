32a690c8e2173263bfd41b41f7e53f3461184f00
Alexander Noriega <alexander@otavialabs.com>
2013-05-28 13:51:13 -0300

    Remove comments for size function which is no longer provided

--- public/examples/Elements/Size.elm
+++ public/examples/Elements/Size.elm
@@ -1,14 +1,8 @@
-
 {-------------------------------------------------------
   You can set the width and height of the element with
   the following two functions:
 
         width, height :: Int -> Element -> Element
-
-  You can set both width and height at the same time
-  with this function:
-  
-          size :: Int -> Int -> Element -> Element
 
    Try them out on Yogi Bear.
 -------------------------------------------------------}
