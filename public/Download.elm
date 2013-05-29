7e3a4f8c8bda07be84688c56a730c1b693b3b6ba
Evan Czaplicki <info@elm-lang.org>
2013-05-29 14:35:44 -0700

    Clean up download page

--- public/Download.elm
+++ public/Download.elm
@@ -3,19 +3,16 @@ import Window
 
 install = [markdown|
 
-### Installation
+### Install
 
-What you are getting:
+Use these [install instructions][1] to get Elm running on your machine.
+You are getting:
 
-1. A **compiler** that turns Elm code into HTML, CSS, and JavaScript. You can
-   then serve those files however you see fit.
+1. **Compiler** &ndash; Turn Elm code into HTML, CSS, and JavaScript.
 
-2. A **self-contained server** that serves Elm files, images, videos, HTML, JavaScript,
-   and anything else you can think of with no set up.
-   No need to mess around with Apache or PHP or Ruby or whatever else.
-   The server just works, so you can focus on things that matter.
+2. **Server** &ndash; Serve Elm files, images, videos, HTML, JavaScript,
+   and anything else you can think of.
 
-See these [install instructions][1] to get Elm running on your machine.
 If you run into problems, you should email the [mailing list][2]
 or report an issue to Elm's [source repository][3]
 
@@ -27,7 +24,7 @@ or report an issue to Elm's [source repository][3]
 
 other = [markdown|
 
-### Source Code
+### Build from Source
 
 The [source code of the Elm compiler and server](https://github.com/evancz/Elm)
 is available, so please browse, fork, and contribute to the project.
@@ -38,17 +35,6 @@ running on your own machine and use Elm's interactive editor locally. From there
 you can use it as a starting point for creating your own website!
 
  [instruct]: https://github.com/evancz/elm-lang.org#elm-langorg-a-template-for-creating-websites-in-elm "install"
-
-<br/>
-
-### Thesis on Elm
-
-My [thesis on Elm][4] is available too. It provides a more formal
-definition of Elm and a description of Concurrent FRP, a new approach
-to efficient [Functional Reactive Programming][frp].
-
-  [4]: http://www.testblogpleaseignore.com/wp-content/uploads/2012/04/thesis.pdf "thesis on Elm"
-  [frp]: /learn/What-is-FRP.elm "functional reactive programming"
 
 |]
 
