8302fe45c16ab674ce72eccf232eb3c721d4e80d
Evan Czaplicki <info@elm-lang.org>
2013-06-04 11:01:06 -0700

    Make landing page and Download page friendlier and more information rich.

--- public/Download.elm
+++ public/Download.elm
@@ -5,20 +5,16 @@ install = [markdown|
 
 ### Install
 
-Use these [install instructions][1] to get Elm running on your machine.
+If you are on Mac OSX, [download this](https://dl.dropboxusercontent.com/u/5850974/Elm/Elm.pkg).
+Otherwise use these [install instructions][1].
 You are getting:
 
-1. **Compiler** &ndash; Turn Elm code into HTML, CSS, and JavaScript.
+1. *Compiler* &ndash; Turn Elm code into HTML, CSS, and JavaScript.
 
-2. **Server** &ndash; Serve Elm files, images, videos, HTML, JavaScript,
+2. *Server* &ndash; Serve Elm files, images, videos, HTML, JavaScript,
    and anything else you can think of.
 
-If you run into problems, you should email the [mailing list][2]
-or report an issue to Elm's [source repository][3]
-
   [1]: https://github.com/evancz/Elm/blob/master/README.md#install "install instructions"
-  [2]: https://groups.google.com/forum/?fromgroups#!forum/elm-discuss "email list"
-  [3]: https://github.com/evancz/Elm "source repository"
 
 |]
 
@@ -26,20 +22,50 @@ other = [markdown|
 
 ### Build from Source
 
-The [source code of the Elm compiler and server](https://github.com/evancz/Elm)
-is available, so please browse, fork, and contribute to the project.
+[The Elm compiler and server](https://github.com/evancz/Elm) are on github.
 
-The [source code of this website](https://github.com/evancz/elm-lang.org) is also
-available. Follow [these instructions][instruct] and you can easily get this entire website
-running on your own machine and use Elm's interactive editor locally. From there
-you can use it as a starting point for creating your own website!
+[This website](https://github.com/evancz/elm-lang.org) is also
+available with [setup instructions][instruct]. The server can
+be the basis for your own website. It also lets you use
+the interactive editor locally.
 
  [instruct]: https://github.com/evancz/elm-lang.org#elm-langorg-a-template-for-creating-websites-in-elm "install"
 
 |]
 
+syntax = [markdown|
+
+### Syntax Highlighting
+
+There are some Elm specific highlighters.
+Haskell syntax highlighting also tends to work pretty well.
+
+* *Vim* &ndash; use Haskell mode or [try elm.vim](https://github.com/otavialabs/elm.vim).
+* *Sublime Text* &ndash; use [the code in this thread](https://groups.google.com/forum/?fromgroups#!searchin/elm-discuss/sublime/elm-discuss/g2T_R-fjF9g/MdugI-8aas0J).
+* *Emacs* &ndash; turn on [haskel-mode](https://github.com/afeinberg/haskellmode-emacs#readme)
+  for `.elm` files.
+
+There is some more info on this [on the email list](https://groups.google.com/forum/?fromgroups#!topic/elm-discuss/Tt0Z538Xv7g).
+|]
+
+problems = [markdown|
+
+### Problems?
+
+If you run into problems, you should email the [mailing list][2], ask
+questions [on IRC](http://webchat.freenode.net/?channels=elm), or
+report an issue to Elm's [source repository][3]
+
+  [2]: https://groups.google.com/forum/?fromgroups#!forum/elm-discuss "email list"
+  [3]: https://github.com/evancz/Elm "source repository"
+
+|]
+
 info w =
   let hwidth = if w < 800 then w `div` 2 - 20 else 380 in
-  flow right [ width hwidth install, spacer 40 10, width hwidth other ]
+  flow down
+    [ flow right [ width hwidth install, spacer 40 10, width hwidth syntax ]
+    , spacer w 30
+    , flow right [ width hwidth other, spacer 40 10, width hwidth problems ] ]
 
 main = lift (skeleton info) Window.width
--- public/Elm.elm
+++ public/Elm.elm
@@ -29,18 +29,23 @@ exampleText = [markdown|
 
 #### Learn by Example
 
+The best way to learn Elm is [by example](/Examples.elm).
 Start with the [basics](/examples/Basic.elm). Work up to
 [intermediate examples](/examples/Intermediate.elm) and
 [games](/blog/games-in-elm/part-0/Making-Pong.html).
 
-These examples will illustrate [Functional Reactive Programming][frp], 
-[strong][strong] / [static][static] types with [type inference][infer],
+The [About](/About.elm) and [Docs](/Documentation.elm) tabs
+give even more info about Elm.
+
+Together these resources illustrate the key features of Elm: [Functional Reactive Programming][frp], 
+[strong][strong] [static][static] types with full [type inference][infer],
 [Markdown support](/edit/examples/Elements/Markdown.elm), the
 [core libraries](/Documentation.elm), and the [module system][modules].
 
+
   [frp]:    /learn/What-is-FRP.elm "functional reactive programming"
-  [strong]: http://en.wikipedia.org/wiki/Strong_typing "strongly"
-  [static]: http://en.wikipedia.org/wiki/Type_system#Static_typing "statically"
+  [strong]: http://en.wikipedia.org/wiki/Strong_typing "strong typing"
+  [static]: http://en.wikipedia.org/wiki/Type_system#Static_typing "static typing"
   [infer]:  http://en.wikipedia.org/wiki/Type_inference "type inference"
   [modules]: http://www.testblogpleaseignore.com/2012/06/19/announcing-elm-0-3-modules/ "Module System"
 
@@ -70,24 +75,37 @@ tryOnline = [markdown|
 #### Use the online editor
 
 The [online editor](/try) lets you write and compile Elm without downloading
-anything. It allows automatic compilation, editor customization, and easy
-navigation between basic examples.
+anything. It supports automatic compilation, editor customization, and inline
+documentation.
 |]
 
 contact = [markdown|
 
 #### Community
 
-Elm's [mailing list][7] is the place to go for questions, announcements,
-and discussion. Or check out the IRC channel ([`#elm` on freenode][irc]).
+[Elm's mailing list][7] is the place to go for questions, announcements,
+and discussion. Or check out the IRC channel at [`#elm` on freenode][irc].
 
- [escape]: /learn/Escape-from-Callback-Hell.elm "Escape from Callback Hell"
- [pong]: /blog/games-in-elm/part-0/Making-Pong.html "Pong"
- [v7]: /blog/announce/version-0.7.elm "Extensible Records"
- [video]: http://www.infoq.com/presentations/Elm "Making the Web Functional"
- [radar]: http://radar.oreilly.com/2012/12/emerging-languages-spotlight-elm.html "Emerging Languages Spotlight"
  [7]: https://groups.google.com/forum/?fromgroups#!forum/elm-discuss "mailing list"
  [irc]: http://webchat.freenode.net/?channels=elm "#elm"
+
+|]
+
+news = [markdown|
+
+#### News
+
+* Upcoming talk on Elm at [StrangeLoop 2013][loop] in St. Louis
+* [Elm and Prezi](http://elm-lang.org/blog/announce/Elm-and-Prezi.elm),
+  [Prezi and Elm](http://engineering.prezi.com/blog/2013/05/21/elm-at-prezi/)
+* [Elm 0.8][v8] finally released!
+* Extensible Records in [Elm 0.7][v7]
+* [Elm featured on O'Reilly Radar][radar]
+
+ [v7]: /blog/announce/version-0.7.elm "Extensible Records"
+ [v8]: /blog/announce/version-0.8.elm "Improve Everything"
+ [loop]: https://thestrangeloop.com/sessions/functional-reactive-programming-in-elm "Elm at StrangeLoop"
+ [radar]: http://radar.oreilly.com/2012/12/emerging-languages-spotlight-elm.html "Emerging Languages Spotlight"
 
 |]
 
@@ -123,7 +141,7 @@ info w =
        [ box exampleText content,
          box resources infoq,
          box tryOnline tryElm,
-         width w contact ]
+         box news (flip width contact) ]
 
 main = lift (skeleton info) Window.width
 
