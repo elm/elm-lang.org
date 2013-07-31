0201d749cece0b75102944b2dbced85384b1f57f
Evan Czaplicki <info@elm-lang.org>
2013-07-31 14:26:28 -0700

    cosmetic changes

--- server/Utils.hs
+++ server/Utils.hs
@@ -16,11 +16,11 @@ pageTitle fp = reverse . takeWhile (/='/') . drop 4 $ reverse fp
 googleAnalytics :: Html
 googleAnalytics =
     H.script ! A.type_ "text/javascript" $
-               "var _gaq = _gaq || [];\n\
-               \_gaq.push(['_setAccount', 'UA-25827182-1']);\n\
-               \_gaq.push(['_trackPageview']);\n\
-               \(function() {\n\
-               \  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;\n\
-               \  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';\n\
-               \  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);\n\
-               \})();"
+         "var _gaq = _gaq || [];\n\
+         \_gaq.push(['_setAccount', 'UA-25827182-1']);\n\
+         \_gaq.push(['_trackPageview']);\n\
+         \(function() {\n\
+         \  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;\n\
+         \  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';\n\
+         \  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);\n\
+         \})();"
