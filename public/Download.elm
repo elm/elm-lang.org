ff5f39b1578565192e08756db27bbb7887fbcda4
Evan <evancz@elm-lang.org>
2013-08-10 06:53:36 +0000

    remove specific instructions from download page

--- public/Download.elm
+++ public/Download.elm
@@ -41,7 +41,7 @@ Haskell syntax highlighting also tends to work pretty well.
 * *Vim* &ndash; use Haskell mode or [try elm.vim](https://github.com/otavialabs/elm.vim).
 * *Emacs* &ndash; turn on [haskell-mode](https://github.com/afeinberg/haskellmode-emacs#readme)
   for `.elm` files.
-* *Sublime Text* &ndash; [Elm Language Support](https://github.com/deadfoxygrandpa/Elm.tmLanguage) with [Package Control](http://wbond.net/sublime_packages/package_control).
+* *Sublime Text* &ndash; [Elm Language Support](https://github.com/deadfoxygrandpa/Elm.tmLanguage).
 
 There is some more info on this [on the email list](https://groups.google.com/forum/?fromgroups#!topic/elm-discuss/Tt0Z538Xv7g).
 |]
