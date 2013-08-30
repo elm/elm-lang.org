691e4145d4aab47f77c9322ddab59937d5d08a64
Evan Czaplicki <info@elm-lang.org>
2013-08-30 10:45:05 -0700

    Make colors prettier

--- public/examples/Elements/Shapes.elm
+++ public/examples/Elements/Shapes.elm
@@ -1,7 +1,7 @@
 
 shape n =
   let angle = degrees (30 * toFloat n)
-  in  circle 10 |> filled (hsv angle 1 1)
+  in  circle 10 |> filled (hsv angle 0.9 1)
                 |> move (45 * cos angle, 45 * sin angle)
 
 main = collage 150 150 <| map shape [0..11]
