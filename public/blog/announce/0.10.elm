2321aff3d1ee891f033316949df018e943ce86c5
Evan Czaplicki <info@elm-lang.org>
2013-10-16 23:56:22 -0400

    Shorten string section and add section on docs

--- public/blog/announce/0.10.elm
+++ public/blog/announce/0.10.elm
@@ -50,59 +50,102 @@ code > span.er { font-weight: bold; }
 
 By far the biggest change in this release is a switch to a [native string
 representation](http://docs.elm-lang.org/library/String.elm).
-Otherwise it is mostly smaller bug fixes and additions.
+This release also brings a wide range of bug fixes and minor improvements,
+such as realiasing in type errors and a patch for pattern matching on literals.
+
+Elm needs more than a compiler to be nice to use, so I have also been working
+on improving tools and examples. 
+
+  * **Tools:** [nicer docs](http://docs.elm-lang.org/) and [hot-swapping](/blog/Interactive-Programming.elm)
+  * **Examples:** [TodoFRP](https://github.com/evancz/todofrp) and [docs source code](https://github.com/evancz/docs.elm-lang.org)
+
+For examples, I have been experimenting with &ldquo;traditional webapps&rdquo; to see
+how easy it is to work with lots of input boxes and buttons. The first experiment was
+[TodoFRP](https://github.com/evancz/todofrp) which is instructive but needs more polish.
+The [new documentation](http://docs.elm-lang.org) also has an instant
+search feature that was a great use for FRP. Both left me feeling like Elm can be
+a great fit for traditional webapps, and I am excited to see how far we can push
+in this direction.
+
+Okay, let's talk specifics!
 
 ## Native Strings
 
-Elm no longer represents strings as lists of characters. This release
-moves from the Haskell inspired character lists to a native string
-representation.
+This release moves from the Haskell inspired list of characters to [a native
+string representation](http://docs.elm-lang.org/library/String.elm) that is
+significantly faster and provides many new string-specific functions.
 
 <div style="text-align:center; font-size:2em;">`String ≠ [Char]`</div>
 
 Character lists are relatively slow, and they expose implementation details
-that make it hard to upgrade to a faster representation. Haskell has dealt
-with this by keeping the slow default but adding the `Text` and `ByteString`
-libraries and an `OverloadedStrings` language extension to make this less
-painful. It is early enough for Elm to just make the breaking change and
-have a nicer default.
+that make it hard to upgrade to a faster representation or even optimize for
+common uses of strings. The [new String library](http://docs.elm-lang.org/library/String.elm)
+is properly abstracted so the underlying representation can be
+optimized or changed without changing the API.
 
-This release introduces [a library specifically for strings](http://docs.elm-lang.org/library/String.elm).
-It is significantly faster and provides many new string-specific functions.
-Overall, I have been really happy with this change. In my experience, it leads
-to two kinds of breaking changes, both pretty easy to fix.
+Overall, the changes I had to make to upgrade [elm-lang.org](/) and
+[docs.elm-lang.org](http://docs.elm-lang.org) were fairly minimal.
+In my experience there were two kinds things that I needed to fix.
 
-  1. You will need to swap out `List` functions for their corresponding
-     `String` function. This means `map` becomes `String.map`, `filter`
-     becomes `String.filter`, etc.
+#### 1. Switch to String functions
 
-  2. If you pattern match on character lists, you need to start using the
-     following function to get equivalent behavior:<br/>
-     <div style="text-align:center; font-family: monospace; padding-top: 4px;">[uncons](http://docs.elm-lang.org/library/String.elm#uncons) : String -> Maybe (Char,String)</div>
+You will need to swap out `List` functions for their corresponding
+`String` function. So `map` becomes `String.map`, `filter`
+becomes `String.filter`, etc. This is fairly straight-forward and the
+type checker helps a ton.
 
-In case two, the transformation will look something like this:
+#### 2. Pattern matching with uncons
+
+Pattern matching now happens with
+[`uncons`](http://docs.elm-lang.org/library/String.elm#uncons) which
+destructures strings without exposing any implementation details.
+For example, finding the length of a string looks like this:
 
 ```haskell
-lengthA : [Char] -> Int
-lengthA charList =
-    case charList of
-      hd::tl -> 1 + lengthA tl
-      []     -> 0
+uncons : String -> Maybe (Char,String)
 
-lengthB : String -> Int
-lengthB string =
-    case String.uncons string of
-      Just (hd,tl) -> 1 + lengthB tl
+length string =
+    case uncons string of
+      Just (hd,tl) -> 1 + length tl
       Nothing      -> 0
 ```
 
-Also, note that using [`String.length`](http://docs.elm-lang.org/library/String.elm#length)
-will be asymptotically faster than either `lengthA` or `lengthB`.
+I mean, [`String.length`](http://docs.elm-lang.org/library/String.elm#length)
+is asymptotically faster than this, but the point is that you can still do
+exactly the same stuff as before with minor syntactic changes.
+I should also note that I got this `uncons` trick
+from the many Haskell libraries that did it first&mdash;`Parsec`, `Text`,
+`ByteString`&mdash;and I look forward to seeing it used in parser combinator
+libraries in Elm.
 
-Overall, the changes I had to make for [elm-lang.org](/) and
-[docs.elm-lang.org](http://docs.elm-lang.org) were not terrible and
-the type checker helped a lot. If you have any trouble with the switch,
-please ask about it on the [mailing list](https://groups.google.com/forum/#!forum/elm-discuss).
+If you have any trouble with the switch, please ask about it on the
+[mailing list](https://groups.google.com/forum/#!forum/elm-discuss)!
+
+## Fixes and Improvements
+
+* Matrix2D becomes Transform2D
+* type realiasing
+* fix for pattern literals
+
+## New Documentation
+
+[docs.elm-lang.org](http://docs.elm-lang.org/) is the new home of documentation
+for the standard libraries. It will eventually host docs for community
+libraries too, making it the place to go to discover and learn new libraries.
+
+My favorite part this project is the search bar. It lets you live search the standard
+library for modules, functions, and operators like `<~`. I have not seen something
+like this in other documentation, and I think that is because it would be
+significantly more complicated without FRP. The code for this feature essentially
+says &ldquo;show the search results&rdquo; and it just does the right thing.
+
+This site also includes [a table of operators](http://docs.elm-lang.org/InfixOps.elm) showing the
+precedence and associativity of all infix ops in the standard library. This information is also
+listed right in the documentation now: see [`(+)`](http://docs.elm-lang.org/library/Basics.elm#+) and
+[`(<~)`](http://docs.elm-lang.org/library/Signal.elm#<~).
+
+[The source code for the docs site](https://github.com/evancz/docs.elm-lang.org) is available
+if you want to look into these features or use it as a basis for your own project.
 
 ## Final Notes
 
