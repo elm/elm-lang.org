66f3118169517d351bc81b7366e0739abd5b4768
Evan Czaplicki <info@elm-lang.org>
2013-10-17 13:42:33 -0400

    Add info on infix declarations, contributions, and small fixes

--- public/blog/announce/0.10.elm
+++ public/blog/announce/0.10.elm
@@ -45,23 +45,24 @@ code > span.er { font-weight: bold; }
 </style>
 
 <h1><div style="text-align:center">Elm 0.10
-<div style="font-size:0.5em;font-weight:normal">*Native strings and incremental fixes*</div></div>
+<div style="font-size:0.5em;font-weight:normal">*Native strings and custom infix ops*</div></div>
 </h1>
 
-By far the biggest change in this release is a switch to a [native string
-representation](http://docs.elm-lang.org/library/String.elm).
-This release also brings a wide range of bug fixes and minor improvements,
-such as realiasing in type errors and a patch for pattern matching on literals.
+ * Switch to a [native string representation](http://docs.elm-lang.org/library/String.elm)
+   that is significantly faster.
+ * Allow custom precedence and associativity for infix operators.
+ * Fix silly bugs and make some small improvements.
 
-Elm needs more than a compiler to be nice to use, so I have also been working
-on improving tools and examples. 
+This release also includes a 
 
-  * **Tools:** [nicer docs](http://docs.elm-lang.org/) and [hot-swapping](/blog/Interactive-Programming.elm)
-  * **Examples:** [TodoFRP](https://github.com/evancz/todofrp) and [docs source code](https://github.com/evancz/docs.elm-lang.org)
+docs with mgold, maxsnew, and jsl
 
-For examples, I have been experimenting with &ldquo;traditional webapps&rdquo; to see
-how easy it is to work with lots of input boxes and buttons. The first experiment was
-[TodoFRP](https://github.com/evancz/todofrp) which is instructive but needs more polish.
+alex noriega List.repeat
+
+Elm needs more than a compiler to be nice to use. This includes tools such as
+[nicer docs](http://docs.elm-lang.org/) and [hot-swapping](/blog/Interactive-Programming.elm).
+I have also been experimenting with &ldquo;traditional webapps&rdquo; in Elm. The first
+experiment was [TodoFRP](https://github.com/evancz/todofrp) proved that the basics work.
 The [new documentation](http://docs.elm-lang.org) also has an instant
 search feature that was a great use for FRP. Both left me feeling like Elm can be
 a great fit for traditional webapps, and I am excited to see how far we can push
@@ -121,11 +122,60 @@ libraries in Elm.
 If you have any trouble with the switch, please ask about it on the
 [mailing list](https://groups.google.com/forum/#!forum/elm-discuss)!
 
-## Fixes and Improvements
+## Infix Operators
 
-* Matrix2D becomes Transform2D
-* type realiasing
-* fix for pattern literals
+You now can set the
+[precedence](http://en.wikipedia.org/wiki/Order_of_operations) and
+[associativity](http://en.wikipedia.org/wiki/Operator_associativity)
+of custom infix operorators. This makes it easier to use [embedded
+DSLs](http://c2.com/cgi/wiki?EmbeddedDomainSpecificLanguage). Hypothetical
+examples include D3 bindings and a parsing library ;)
+
+Let's see how it works! Elm&rsquo;s signal operators are defined like this:
+
+```haskell
+f <~ s = lift f s
+sf ~ s = lift2 (<|) sf s
+
+infixl 4 <~
+infixl 4 ~
+```
+
+This declares that the `(<~)` and `(~)` operators are left associative and have
+precedence four. &ldquo;Left associative&rdquo; means parentheses are added from
+the left, so the following expressions are equivalent:
+
+```haskell
+signal  = f <~ a ~ b ~ c
+signal' = (((f <~ a) ~ b) ~ c)
+```
+
+Left associativity is the default because we tend to read from left to right, but
+sometimes right associativity is very useful. Boolean *or* is a great example!
+
+```haskell
+a || b = if a then True else b
+infixr 2 ||
+
+falseLeft  = (True || False) || False
+falseRight = True || (False || False)
+```
+
+Where you add the parentheses does not change the result, but since `(||)`
+[short ciruits](http://en.wikipedia.org/wiki/Short-circuit_evaluation), it
+*does* change how much computation needs to be done. Making `(||)` right
+associative ensures that we use the faster way when parentheses are left off.
+
+You can even do all this with infix functions:
+
+```haskell
+infixl 7 `div`
+
+fortyTwo = 84 `div` 2
+```
+
+Fun fact: [all of the operators in the standard library](http://docs.elm-lang.org/InfixOps.elm)
+are defined this way now. 
 
 ## New Documentation
 
@@ -139,15 +189,40 @@ like this in other documentation, and I think that is because it would be
 significantly more complicated without FRP. The code for this feature essentially
 says &ldquo;show the search results&rdquo; and it just does the right thing.
 
-This site also includes [a table of operators](http://docs.elm-lang.org/InfixOps.elm) showing the
-precedence and associativity of all infix ops in the standard library. This information is also
-listed right in the documentation now: see [`(+)`](http://docs.elm-lang.org/library/Basics.elm#+) and
-[`(<~)`](http://docs.elm-lang.org/library/Signal.elm#<~).
-
 [The source code for the docs site](https://github.com/evancz/docs.elm-lang.org) is available
-if you want to look into these features or use it as a basis for your own project.
+if you want to look into how search works or use the site as a basis for your own project.
 
-## Final Notes
+## Fixes, Improvements, Thank yous
 
+Thanks to everyone who helped with this release, whether it was
+contributions or helping talk through ideas on the [email
+list](https://groups.google.com/forum/#!forum/elm-discuss)! 
+The following list covers the most important fixes and improvements.
+
+* Realiasing type errors, making them shorter and easier to read.
+
+* Fix the `remove` function in [the `Dict`
+  library](http://docs.elm-lang.org/library/Dict.elm) based on [Matt Might's
+  work on this topic](http://matt.might.net/articles/red-black-delete/). Thank you
+  to [Max New](https://github.com/maxsnew) for taking on this arduous task!
+
+* The `Matrix2D` library has been renamed [`Transform2D`](http://docs.elm-lang.org/library/Transform2D.elm).
+  This library is actually made up of [augmented matrices](http://en.wikipedia.org/wiki/Affine_transformation#Augmented_matrix)
+  that let you represent translations, and we wanted to make that clearer.
+
+* Pattern matching on literals was announced in 0.9, but a bug snuck in right before
+  release. That is fixed now!
+
+* Add function to `Random` library to get a list of random numbers, thanks to
+  [Max Goldstien](https://github.com/mgold)!
+  <p style="text-align:center; font-family:monospace;">
+  [floatList](http://docs.elm-lang.org/library/Random.elm#floatList) : Signal Int -> Signal [Float]</p>
+
+* Make compiler compatable with cabal 1.18, thanks to [Justin Leitgeb](https://github.com/jsl)!
+
+* Fix bug in functions that take 10 or more arguments, thanks to [Max New](https://github.com/maxsnew)
+
+* Switch to `language-ecmascript` for generating JS. If you generate JS from Haskell, you
+  should switch too. This is a good library
 
 |]
