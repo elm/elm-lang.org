717a9e04a072d94f2f94f08ea4e99198650dae48
Evan Czaplicki <info@elm-lang.org>
2013-10-18 00:14:29 -0400

    Add a flag for setting the port number for the server

--- server/Server.hs
+++ server/Server.hs
@@ -1,18 +1,20 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, DeriveDataTypeable #-}
 module Main where
 
 import Prelude hiding (head, span, id, catch)
 import qualified Data.Char as Char
 import qualified Data.List as List
 import Control.Monad
-import Happstack.Server hiding (body)
+import Happstack.Server hiding (body,port)
 import Happstack.Server.Compression
 import Happstack.Server.FileServe.BuildingBlocks (serveDirectory')
+import qualified Happstack.Server as Happs
 
 import Text.Blaze.Html (Html, toHtml)
 import Text.Blaze.Html.Renderer.String (renderHtml)
 import Control.Monad.Trans (MonadIO(liftIO))
 import Control.Exception
+import System.Console.CmdArgs
 import System.FilePath as FP
 import System.Process
 import System.Directory
@@ -23,15 +25,24 @@ import ElmToHtml
 import Editor
 import Utils
 
+data Flags = Flags
+  { port :: Int
+  } deriving (Data,Typeable,Show,Eq)
+
+flags = Flags
+  { port = 8000 &= help "set the port of the server"
+  }
+
 -- | Set up the server.
 main :: IO ()
 main = do
   setNumCapabilities =<< getNumProcessors
+  args <- cmdArgs flags
   putStrLn "Initializing Server"
   precompile
   getRuntime
   putStrLn "Serving at localhost:8000"
-  simpleHTTP nullConf $ do
+  simpleHTTP nullConf { Happs.port = port args } $ do
     compressedResponseFilter
     let mime = asContentType "text/html; charset=UTF-8"
     route (serveFile mime "public/build/Elm.elm")
