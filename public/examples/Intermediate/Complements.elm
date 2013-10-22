1b9bf7cdbbe5b8675259ac079700446d698fc2a5
Evan Czaplicki <info@elm-lang.org>
2013-10-22 11:30:35 +0200

    Make this example more fun looking

--- public/examples/Intermediate/Complements.elm
+++ public/examples/Intermediate/Complements.elm
@@ -1,11 +1,8 @@
-
-colorCycle t =
-  let at t   = move <| fromPolar (100, t)
-      toDot r t = at t . filled (hsv t 0.9 0.9) <| circle r
-      t1 = t / 2000
-      t2 = t / 2000 + degrees 180
-  in  collage 300 300 <|
-        toDot 20 t1 :: 
-        toDot 20 t2 :: map (\angle -> toDot 10 (turns <| angle / 12)) [0..11]
-
+ 
 main = lift colorCycle (foldp (+) 0 (fps 30))
+
+colorCycle time =
+  let toDot angle = circle (20 + 10 * sin (angle * 2 + inSeconds time))
+                      |> filled (hsv angle 0.9 0.9)
+                      |> move (fromPolar (100, angle + pi))
+  in  collage 300 300 (map (\n -> toDot <| turns (n/12)) [0..11])
