ab5ccb051f9bb92cbc2f6df9d2357c81cbcbb11f
Evan Czaplicki <info@elm-lang.org>
2013-10-25 17:56:47 +0200

    Make the clock "prettier"
    @laszlopandy, I can't spare any more lines for aesthetics or we'll be
    in double digits! :P

--- public/examples/Intermediate/Clock.elm
+++ public/examples/Intermediate/Clock.elm
@@ -1,11 +1,11 @@
--- An 8-line analog clock.
 
 main = lift clock (every second)
 
-clock t = collage 400 400 [ filled grey (ngon 12 110)
-                          , hand orange 100  t
-                          , hand  blue  100 (t/60)
-                          , hand  blue  60  (t/720) ]
+clock t = collage 400 400 [ filled    lightGrey   (ngon 12 110)
+                          , outlined (solid grey) (ngon 12 110)
+                          , hand orange   100  t
+                          , hand charcoal 100 (t/60)
+                          , hand charcoal 60  (t/720) ]
 
 hand clr len time =
   let angle = degrees (90 - 6 * inSeconds time)
