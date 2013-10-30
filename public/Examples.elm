26f801ae7a6e2ac26324a0a29e4c77d66157edae
Dénes Harmath <harmathdenes@gmail.com>
2013-10-31 00:13:10 +0100

    Fix link

--- public/Examples.elm
+++ public/Examples.elm
@@ -1,4 +1,3 @@
-
 import Website.Skeleton (skeleton)
 import Website.ColorScheme (accent4)
 import Website.Tiles (examples)
@@ -55,7 +54,7 @@ for building on top of or retrofitting for your purposes.
  * [Tetris](https://github.com/jcollard/elmtris) &ndash;
    by Joe Collard
 
- * [Breakout](https://github.com/jcollard/elmtris) &ndash;
+ * [Breakout](https://github.com/Dobiasd/Breakout) &ndash;
    by Tobias Hermann
 
  * [Celestia](https://github.com/johnpmayer/celestia) &ndash;
