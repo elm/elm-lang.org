612f6dde4b89a8bd814ff57d07272aaafea9a70a
Thomas Bereknyei <tomberek@gmail.com>
2013-10-31 23:40:02 -0400

    Fixed Form.elm

--- public/examples/Intermediate/Form.elm
+++ public/examples/Intermediate/Form.elm
@@ -1,65 +1,78 @@
-
 module Form where
 
-import JavaScript
-
+import open Graphics.Input
+import open Http
 
 -- Helpers
+isEmptyString : String -> Bool
+isEmptyString x = x == ""
 
-isEmpty xs = case xs of { [] -> True ; _ -> False }
-
+getErrors : String -> String -> String -> String -> [String]
 getErrors first last email remail =
-  Maybe.justs <| map (\(err,msg) -> if err then Just msg else Nothing)
-  [ (isEmpty first  , "First name required.")
-  , (isEmpty last   , "Last name required.")
-  , (isEmpty email  , "Must enter your email address.")
-  , (isEmpty remail , "Must re-enter your email address.")
+  justs <| map (\(err,msg) -> if err then Just msg else Nothing)
+  [ (isEmptyString first  , "First name required.")
+  , (isEmptyString last   , "Last name required.")
+  , (isEmptyString email  , "Must enter your email address.")
+  , (isEmptyString remail , "Must re-enter your email address.")
   , (email /= remail, "Email addresses do not match.")
   ]
 
-url first last email =
-  "login?first=" ++ first ++ "&last=" ++ last ++ "&email="++ email
-
+url : String -> String -> String -> String
+url first last email = 
+    "http://elm-lang.org/login?first=" ++ first ++ "&last=" ++ last ++ "&email="++ email
 
 -- Signals
+(firstBox , first)  = field "First Name"
+(lastBox  , last)   = field "Last Name"
+(emailBox , email)  = field "Your Email"
+(remailBox, remail) = field "Re-enter Email"
+(butn     , press)  = button "Submit"
 
-(firstBox , first)  = Input.textField "First Name"
-(lastBox  , last)   = Input.textField "Last Name"
-(emailBox , email)  = Input.textField "Your Email"
-(remailBox, remail) = Input.textField "Re-enter Email"
-(butn     , press)  = Input.button "Submit"
-
-pressCount = countIf id press
+errors : Signal [String]
 errors = lift4 getErrors first last email remail
-sendable = lift2 (&&) press (lift isEmpty errors)
 
-redirectTo = lift castStringToJSString
-                  (keepWhen sendable "" (lift3 url first last email))
-
-foreign export jsevent "elm_redirect"
-  redirectTo : Signal JSString
-
+sendable : Signal Bool
+sendable = sampleOn press <| lift isEmpty errors
 
 -- Display
-
-field txt fld =
+fieldWith : String -> Element -> Element
+fieldWith txt fld =
   flow right
     [ container 120 32 midRight <| plainText txt
     , container 200 32 middle <| size 180 26 fld ]
 
-showErrors presses errs =
-  if presses == 0 || isEmpty errs then spacer 10 10 else
+showErrors : [String] -> Element
+showErrors errs =
+  if isEmpty errs then spacer 10 10 else
     flow down <| map (text . Text.color red . toText) errs
 
-form presses errs =
-  let entry = color (rgb 230 230 230) . flow down <|
-               [ field "First Name:"     firstBox
-               , field "Last Name:"      lastBox
-               , field "Your Email:"     emailBox
-               , field "Re-enter Email:" remailBox
-               , showErrors presses errs
+entry : Element -> Element -> Element -> Element -> [String] -> Element
+entry f l em r e = (color (rgb 230 230 230) . flow down) <|
+               [ fieldWith "First Name:" f
+               , fieldWith "Last Name:"  l
+               , fieldWith "Your Email:" em
+               , fieldWith "Re-enter Email:" r
+               , showErrors e
                , container 310 40 midRight <| size 60 30 butn
                ]
-  in  container (widthOf entry + 60) (heightOf entry + 60) middle entry 
 
-main = lift2 form pressCount errors
+-- HTTP control
+sendRequest : Signal String
+sendRequest = keepWhen sendable "" <| lift3 url first last email
+
+getLogin : Signal String -> Signal (Response String)
+getLogin req = send <| lift (\r -> post r "") req
+
+-- HTTP printing  
+prettyPrint : Response String -> Element
+prettyPrint res = case res of
+  Waiting -> plainText ""
+  Failure _ _ -> plainText ""
+  Success a -> plainText a
+
+main : Signal Element
+main = 
+  above
+    <~ (lift (container 360 360 topLeft) <| entry <~ firstBox ~ lastBox ~ emailBox ~ remailBox ~ errors)
+     ~ (lift prettyPrint <| getLogin sendRequest)
+
