a3b63eae21e345fbb002f04f0d825c6fe2d1e59c
Mads Flensted-Urech <mfu@coadu.com>
2014-01-06 16:04:48 +0100

    Use 'canvas' instead of 'viewport' and backticks around codesnippets

--- public/learn/From-other-language-tips.elm
+++ public/learn/From-other-language-tips.elm
@@ -22,10 +22,10 @@ fromHaskell = [markdown|
 #### From Haskell
 
 * Elm is not Haskell. Although the Elm compiler is written in Haskell, and  Haskell, along with other functional langages, is a source of insipiration for the Elm language and syntax.
-* Elm's (<|) operator is equivalent to Haskell's ($). There is also (|>) which has the arguments reversed. The arrow points towards the function.
+* Elm's `(<|)` operator is equivalent to Haskell's `($)`. There is also `(|>)` which has the arguments reversed. The arrow points towards the function.
 * The order of foldl's arguments is deliberately different from Haskell's to improve composability in Elm.
-* The meanings of (:) and (::) are swapped. (:) is “has type” and (::) is cons. This is the same as ML.
-* Graphics.Collage uses Cartesian coordinates with the origin at the center of the viewport, instead of having (0,0) at top-left corner. Positive y is up.
+* The meanings of `(:)` and `(::)` are swapped. `(:)` is “has type” and `(::)` is cons. This is the same as ML.
+* Graphics.Collage uses Cartesian coordinates with the origin at the center of the canvas, instead of having (0,0) at top-left corner. Positive y is up.
 * Elm is not lazy
 
 |]
@@ -34,12 +34,12 @@ fromJS = [markdown|
 
 #### From JS, HTML and CSS
 
-* Graphics.Collage uses Cartesian coordinates with the origin at the center of the viewport, instead of having (0,0) at top-left corner. Positive y is up.
+* Graphics.Collage uses Cartesian coordinates with the origin at the center of the canvas, instead of having (0,0) at top-left corner. Positive y is up.
 * The generated HTML is not intended for external styling. Learn about [Elm Graphics](/learn/courses/beginner/Graphics.elm) and try [these examples](/Examples.elm#Display)
 * The Elm compiler enforces type-correctness and does not do automatic casting.
 * Lists use square brackets like arrays, but all items must be of the same type. They are implemented as singly-linked linked lists.
-* Tuples such as (3, True) may mix types but must be of a known, fixed length.
-* Anonymous functions are written (\var1 var2 -> expression).
+* Tuples such as `(3, True)` may mix types but must be of a known, fixed length.
+* Anonymous functions are written `(\var1 var2 -> expression)`.
 * Function application happens before any infix operation.
 
 |]
