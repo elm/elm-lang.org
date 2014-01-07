d49d005cc56db9b1b0dedac6bae15e11d53e7532
Evan Czaplicki <info@elm-lang.org>
2014-01-07 11:25:59 +0100

    Flesh out some arguments and add some links

--- public/learn/From-other-language-tips.elm
+++ public/learn/From-other-language-tips.elm
@@ -21,26 +21,43 @@ fromHaskell = [markdown|
 
 #### From Haskell
 
-* Elm is not Haskell. Although the Elm compiler is written in Haskell, and  Haskell, along with other functional langages, is a source of insipiration for the Elm language and syntax.
-* Elm's `(<|)` operator is equivalent to Haskell's `($)`. There is also `(|>)` which has the arguments reversed. The arrow points towards the function.
-* The order of foldl's arguments is deliberately different from Haskell's to improve composability in Elm.
-* The meanings of `(:)` and `(::)` are swapped. `(:)` is “has type” and `(::)` is cons. This is the same as ML.
-* Graphics.Collage uses Cartesian coordinates with the origin at the center of the canvas, instead of having (0,0) at top-left corner. Positive y is up.
-* Elm is not lazy.
-* Signals are not monads. There is no function `join : Signal (Signal a) -> Signal a`. This is done for performance reasons.
-
+* [Although Elm looks like Haskell, Elm is not
+  Haskell](http://www.testblogpleaseignore.com/2012/06/21/why-elm/). In the pursuit
+  of making purely functional GUIs easy and practical, Elm draws inspiration from
+  many languages (e.g. Haskell, OCaml, Agda, F#, SML) and aims to borrow and improve
+  upon the best of existing languages and research.
+* Instead of using `($)` for function application, Elm uses the `(<|)` and `(|>)` operators for
+  forward and backward application: `(f <| x) == f x == (x |> f)`. Borrowed from F#, the goal is
+  to make the symbol reflect its meaning of "give this argument to that function".
+* The order of arguments for [foldl](http://library.elm-lang.org/catalog/evancz-Elm/0.10.1/List#foldl)
+  is deliberately different from Haskell's [to improve composability](http://library.elm-lang.org/DesignGuidelines.html#the-data-structure-is-always-the-last-argument).
+* The meanings of `(:)` and `(::)` are swapped, `(:)` is “has type” and `(::)` is cons.
+  This follows in the tradition of ML, Standard ML, and OCaml. It also matches the prevailing
+  trend in more recent languages such as F#, Agda, Rust, etc. to use the single colon mean "has type"
+  because it is much more common case than consing.
+* [Graphics.Collage](http://library.elm-lang.org/catalog/evancz-Elm/0.10.1/Graphics-Collage)
+  uses Cartesian coordinates, so positive y is up and the origin at the center of the canvas.
+* [Elm is not lazy](http://www.testblogpleaseignore.com/2012/06/22/the-trouble-with-frp-and-laziness/).
+* Signals are not monads. There is no function `join : Signal (Signal a) -> Signal a`. As of 2013,
+  pure monadic FRP still has extremely serious unsolved theoretical problems. The short version is that
+  memory usage must grow linearly with time to maintain referential transparency. You can read more
+  about these concerns in [my thesis](http://www.testblogpleaseignore.com/wp-content/uploads/2012/04/thesis.pdf)
+  or the closely related [PLDI paper](http://people.seas.harvard.edu/~chong/abstracts/CzaplickiC13.html).
 |]
 
 fromJS = [markdown|
 
 #### From JS, HTML and CSS
 
-* Graphics.Collage uses Cartesian coordinates with the origin at the center of the canvas, instead of having (0,0) at top-left corner. Positive y is up.
-* The generated HTML is not intended for external styling. Learn about [Elm Graphics](/learn/courses/beginner/Graphics.elm) and try [these examples](/Examples.elm#Display)
+* [Graphics.Collage](http://library.elm-lang.org/catalog/evancz-Elm/0.10.1/Graphics-Collage)
+  uses Cartesian coordinates, so positive y is up and the origin at the center of the canvas
+* The generated HTML is not intended for external styling. Learn about
+  [Elm Graphics](/learn/courses/beginner/Graphics.elm) and try [these examples](/Examples.elm#Display)
 * The Elm compiler enforces type-correctness and does not do automatic casting.
-* Lists use square brackets like arrays, but all items must be of the same type. They are implemented as singly-linked linked lists.
+* Lists use square brackets like arrays, but all items must be of the same type.
+  They are implemented as singly-linked linked lists.
 * Tuples such as `(3, True)` may mix types but must be of a known, fixed length.
-* Anonymous functions are written `(\var1 var2 -> expression)`.
+* Anonymous functions are written `(\arg1 arg2 -> expression)`.
 * Function application happens before any infix operation.
 
 |]
