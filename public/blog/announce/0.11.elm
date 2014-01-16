d056341b685df9524a77bd193e8c6485db42ec35
Evan Czaplicki <info@elm-lang.org>
2014-01-16 19:02:08 +0100

    Start announcement for 0.11, all about ports

---
+++ public/blog/announce/0.11.elm
@@ -0,0 +1,46 @@
+
+import Website.Skeleton (skeleton)
+import open Website.ColorScheme
+import Window
+import JavaScript as JS
+
+port title : String
+port title = "Elm 0.11 - Ports"
+
+main = lift (skeleton everything) Window.dimensions
+
+everything wid =
+    let w = min 600 wid
+    in  width w intro
+
+intro = [markdown|
+
+<style type="text/css">
+p { text-align: justify }
+pre { background-color: white;
+      padding: 10px;
+      border: 1px solid rgb(216, 221, 225);
+      border-radius: 4px;
+}
+code > span.kw { color: #204a87; font-weight: bold; }
+code > span.dt { color: #204a87; }
+code > span.dv { color: #0000cf; }
+code > span.bn { color: #0000cf; }
+code > span.fl { color: #0000cf; }
+code > span.ch { color: #4e9a06; }
+code > span.st { color: #4e9a06; }
+code > span.co { color: #8f5902; font-style: italic; }
+code > span.ot { color: #8f5902; }
+code > span.al { color: #ef2929; }
+code > span.fu { color: #000000; }
+code > span.er { font-weight: bold; }
+</style>
+
+<h1><div style="text-align:center">Elm 0.11 &ndash; Ports
+<div style="padding-top:4px;font-size:0.5em;font-weight:normal">*A better JavaScript FFI and the &ldquo;Component Model&rdquo;*</div></div>
+</h1>
+
+This release makes it extremely simple to use Elm and JavaScript together.
+
+
+|]
