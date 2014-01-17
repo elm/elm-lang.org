d31a6accb34be64f2c25374ade615a86603acebd
Evan Czaplicki <info@elm-lang.org>
2014-01-17 17:34:59 +0100

    Add lots more info to the 0.11 announcement

--- public/blog/announce/0.11.elm
+++ public/blog/announce/0.11.elm
@@ -36,8 +36,8 @@ code > span.fu { color: #000000; }
 code > span.er { font-weight: bold; }
 </style>
 
-<h1><div style="text-align:center">Elm 0.11 &ndash; greatly improved FFI
-<div style="padding-top:4px;font-size:0.5em;font-weight:normal">*Ports and the &ldquo;Component Model&rdquo;*</div></div>
+<h1><div style="text-align:center">Elm 0.11 &ndash; Ports
+<div style="padding-top:4px;font-size:0.5em;font-weight:normal">*A drastically improved FFI and the &ldquo;Component Model&rdquo;*</div></div>
 </h1>
 
 The key idea for this release comes from a “component model” for using Elm in
@@ -49,13 +49,23 @@ Elm out on a specific problem and see if it works for you.
 <img src="/imgs/embed.png" alt="Component Model"
      style="width:480px; height:320px; margin-left: auto; margin-right: auto; display:block;">
 
+To make this “component model” possible, Elm 0.11 introduces
+[ports](/learn/Ports.elm) which make it easy to
+communicate between Elm and JavaScript. Check out the documents on
+[how to embed Elm in HTML](/learn/Embed-in-HTML.elm) and [how to
+communicate between Elm and JS](/learn/Ports.elm) to learn the specifics
+of these new features. This post will focus on why ports are extremely
+important for Elm and how to understand and use ports effectively.
+
+## Importance of the “component model”
+
 The success stories I have heard for JVM languages like Scala and Clojure
 go something like this: “There was some module that was a terrible
 quagmire of state and bugs and slowness. [There did not appear to be any
 solution](http://youtu.be/WzEhoyXpqzQ?t=31s). An individual rewrote it in
 language X. It was a third as long, ran faster, and was easier to maintain.
 There was much rejoicing and language X became the primary language of that team.”
-The key components are:
+The key components of these stories are:
 
  * There was a known problem that seemed intractable.
  * The problem was bad but small enough for one person to fix.
@@ -68,12 +78,139 @@ problems that are particularly well-suited to purity, immutability,
 reactivity, type-safety, etc. I think there is still more to improve in
 Elm, but the “component model” at least makes this kind of story *possible*.
 
-## Ports
+## Introduction to Ports
 
-Before this release the FFI was pretty intense and unweildy. It required lots of
-keywords, felt clunky, and was limited to signals. This release introduces
-[ports](/learn/Ports.elm), a cleaner and more general FFI.
+Here is a simple module `FinancialAdvisor` that uses ports to import stock
+prices and export buy orders.
 
+```haskell
+module FinancialAdvisor where
 
+-- incoming signal of prices
+port prices : Signal Float
+
+-- outgoing signal of buy orders
+port orders : Signal String
+port orders = constant "BRK.A"
+```
+
+All ports are declared with the `port` keyword. Incoming ports are just a
+name and a type annotation. In the example above, the `prices` port is
+a signal of floats that will be sent in from JS. Outgoing ports require
+a name, type annotation, and *definition*. A definition indicates that
+the value should be sent *out* to JS. So the example above outlines a very
+simplistic investment strategy:
+
+ 1. Look at incoming stock prices.
+ 2. Ignore those stock prices.
+ 3. Buy [Berkshire Hathaway class A shares](https://www.google.com/finance?q=NYSE:BRK.A).
+
+Now all that you need to do is initialize this winning investment strategy
+from JS:
+
+```javascript
+// initialize the component, specifying all incoming ports
+var advisor = Elm.worker(Elm.FinancialAdvisor, { prices:100 });
+
+// send some prices to the component
+advisor.ports.prices.send(103);
+advisor.ports.prices.send(94);
+advisor.ports.prices.send(99);
+
+function buy(stockTicker) {
+    // create a function to act on buy orders
+}
+
+// act on the latest financial advice
+advisor.ports.orders.subscribe(buy);
+
+// decide that this is not a good investment strategy
+advisor.ports.orders.unsubscribe(buy);
+```
+
+One could make this component more sophisticated by actually doing some
+analysis of the incoming prices to guide the buy orders. That might be
+tough to do well, but at least sending values between Elm and JavaScript
+is easy now!
+
+## Customs and Border Protection
+
+Ports must be careful about what values are allowed through.
+Elm is statically typed, so each port is fitted with some
+border protection code that ensures that type errors are kept
+out. Ports also do some conversions so that you get nice
+colloqial data structures in both Elm and JS.
+
+The particular types that can be sent in and out of ports is
+actually quite flexible. It covers pretty much [all valid JSON values](http://www.json.org/). Incoming
+ports can handle [all JS values](http://library.elm-lang.org/catalog/evancz-Elm/0.11/JavaScript)
+and the following Elm types:
+
+  * **Booleans and Strings** &ndash; both exist in Elm and JS!
+  * **Numbers** &ndash; Elm ints and floats correspond to JS numbers
+  * **Lists**   &ndash; correspond to JS arrays
+  * **Tuples**  &ndash; correspond to fixed-length, mixed-type JS arrays
+  * **Records** &ndash; correspond to JavaScript objects
+  * **Signals** &ndash; correspond to event streams in JS
+  * **Maybes**  &ndash; `Nothing` and `Just 42` correspond to `null` and `42` in JS
+
+All conversions are symmetric and type safe. If someone tries to give a
+badly typed value to Elm it will throw an error in JS immediately. By having
+a strict border check like this, the Elm can continue to guarantee that
+you will never have type errors at runtime.
+
+Outgoing ports let you export all of the values listed above with
+one important addition: first-order functions!
+If you wrote a nice parser or library in Elm, you can use those functions
+directly in JS, whether that is in a browser or in [node.js](http://nodejs.org/).
+The mapping between Elm and JS function looks like this:
+
+    add x y = x + y
+
+    function add(x,y) { return x + y; }
+
+You lose currying on the JS side, but the goal of this whole feature is to
+produce *colloqial* values in both Elm and JS. One important restriction on
+exporting functions is that they must be *first-order* functions. Things
+like `map` and `foldl` cannot be exported because the Elm compiler may
+eventually perform optimizations that assume purity, and higher-order
+functions allow you to introduce impure functions which *could* be executed
+in an unexpected order. (Thanks to Max New for pointing this out!)
+
+## Design goals and inspiration for Ports
+
+Before this release, talking to JS was pretty intense and unweildy. It required
+lots of keywords, felt clunky, and was limited to signals. It was technically
+possible to use Elm in a component model, but it just felt onerous.
+So the biggest goal for this API was to make it easy and fun to
+communicate between Elm and JS.
+
+This syntax for ports was chosen after tons of iterations and brainstorms
+on syntax. Seriously, we tried *a lot* of different syntax, and I think
+this one is uniquely light without giving up too much clarity.
+
+The underlying idea was inspired by message-passing concurrency and
+[flow-based programming](http://en.wikipedia.org/wiki/Flow-based_programming).
+I think these concepts give a good context for thinking about and using ports:
+
+* **Message-passing concurrency**:
+  [Concurrent ML](http://people.cs.uchicago.edu/~jhr/papers/2009/icfp-parallel-cml.pdf)
+  was extremely influential in [the theoretical work underlying
+  Elm](http://www.testblogpleaseignore.com/wp-content/uploads/2012/04/thesis.pdf),
+  and their key abstraction is *channels* which allow messages to be passed between
+  components that run concurrently know nothing about each other. A similar model is
+  used in Go and Erlang. I cannot say enough good things about this style of
+  programming, and I really hope it becomes more known and accepted in the JS
+  community.
+
+* **Flow-based Programming**: I was mostly inspired by the strong visual language
+  of [flow-based programming](http://en.wikipedia.org/wiki/Flow-based_programming).
+  Although the diagrams I see are usually [sad looking UML with randomly selected
+  colors schemes](http://en.wikipedia.org/wiki/File:FBP_-_Simple_network.png), they
+  make the core concepts of a “component model” extremely clear and have the *potential*
+  to be beautiful. This is also where I got the term “port”.
+
+So although ports let you import and export non-signal values (which can be super handy
+at times), the primary intent of ports is to deal with signals.
 
 |]
