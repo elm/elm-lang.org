c3c4522a8991cc56ab03b644aa5c463193c9899f
madscoaducom <mads@coadu.com>
2014-01-20 08:22:22 +0100

    Use cartesian coordinates

--- public/examples/Reactive/Draw.elm
+++ public/examples/Reactive/Draw.elm
@@ -1,5 +1,4 @@
--- Try this out on an iOS or Android device. For best results
--- use the "In Tab" compile option.
+-- Try this out on an iOS or Android device.
 
 import Dict
 import Touch
@@ -12,6 +11,10 @@ add1 t d = let vs = Dict.findWithDefault [] t.id d
 addN : [Touch.Touch] -> Dict.Dict Int [(Int,Int)] -> Dict.Dict Int [(Int,Int)]
 addN ts dict = foldl add1 dict ts
 
+cartesianTouch : (Int, Int) -> [Touch.Touch] -> [Touch.Touch]
+cartesianTouch (w, h) ts = let transformTouch t = { t | x <- t.x - (div w 2), y <- -t.y + (div h 2) }
+                           in map transformTouch ts
+
 thickLine : LineStyle
 thickLine = { defaultLine | color <- rgba 123 123 123 0.3, width <- 8 }
 
@@ -19,7 +22,10 @@ scene : (Int,Int) -> [[(Int,Int)]] -> Element
 scene (w,h) paths =
     let float (a,b) = (toFloat a, toFloat b)
         pathForms = group (map (traced thickLine . path . map float) paths)
-    in  collage w h [ move (float (-w `div` 2, h `div` 2)) pathForms ]
+    in  collage w h [ pathForms ]
+
+cartesianTouches : Signal [Touch.Touch]
+cartesianTouches = cartesianTouch <~ Window.dimensions ~ Touch.touches
 
 main = lift2 scene Window.dimensions
-                   (Dict.values <~ foldp addN Dict.empty Touch.touches)
+                   (Dict.values <~ foldp addN Dict.empty cartesianTouches)
