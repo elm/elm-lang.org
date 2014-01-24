23d1362adc62b4df70a3f9371540b165d08dc86a
Evan Czaplicki <info@elm-lang.org>
2014-01-24 03:29:50 +0100

    Try to make the website easier to navigate and make important things easier to find

--- public/Learn.elm
+++ public/Learn.elm
@@ -1,8 +1,6 @@
 import Website.Skeleton (skeleton)
 import Window
 
----- Text of the page: all written in Markdown ----
-
 intro = [markdown|
 <style>
 h1 { margin-bottom: 0; }
@@ -13,35 +11,23 @@ h5 { margin-bottom: 0.5em; }
 
 # Learn
 
-This page focuses on written and video tutorials on Elm. There are
-learning materials for every level of programmer:
-
- * *New to programming* &mdash; focus on the [beginner classes](#beginner-classes)
-
- * *New to functional programming* &mdash; check out the
-   [mini tutorials](#mini-tutorials), [syntax reference](/learn/Syntax.elm),
-   and [these examples](/Examples.elm#compute)
-
- * *New to FRP* &mdash; read [this intro](/learn/What-is-FRP.elm),
-   see [these examples](/Examples.elm#react),
-   watch [this video](http://www.ustream.tv/recorded/29330499),
-   and read [this paper](http://www.testblogpleaseignore.com/wp-content/uploads/2012/04/thesis.pdf)
-
- * *Coming to Elm from another programming language?* &mdash; see these [Tips](/learn/From-other-language-tips.elm)
-
- * *Using Elm* &mdash; see the [intermediate and large examples](/Examples.elm)
-   and resources on [making stuff](#making-stuff), [formal semantics](#publications),
-   [release notes](/Install.elm#release-notes),
-   and [blog posts](#articles-blog)
+The [syntax reference](/learn/Syntax.elm) is a good place to start.
+The [examples page](/Examples.elm) is intended to help you learn by
+example, slowly building up skills. From there, this page provides
+much more documentation. See [the roadmap](/learn/Roadmap.elm) for
+advice on how to learn Elm based on your background.
 
 |]
 
 leftCol = [markdown|
 
-#### Mini Tutorials
+#### Syntax
+
+* [Syntax reference](/learn/Syntax.elm)
+
+#### Features
 
 * [FRP](/learn/What-is-FRP.elm)
-* [Syntax](/learn/Syntax.elm)
 * [Types](/learn/Getting-started-with-Types.elm)
 * [Algebraic Data Types](/learn/Pattern-Matching.elm)
 * [Records](/learn/Records.elm)
@@ -70,16 +56,21 @@ papers = [markdown|
 
 #### Conference Videos
 
-* [Making the Web Functional][infoq] &ndash; Elm and purely functional graphics
-* [Functional Reactive Programming][mlocjs] &ndash; intro to FRP, live-coding Mario
+* [StrangeLoop 2013][sl2013] &mdash; The best one so far! Teaches the basics of
+  graphics and FRP in Elm and builds up to implementing a basic [Mario
+  game](/edit/examples/Intermediate/Mario.elm) *and* it is pretty fun to watch.
+* [mloc.js 2013][mlocjs] &mdash; a less polished version of the StrangeLoop 2013 talk.
+* [StrangeLoop 2012][sl2012] &mdash; First conference talk ever. Covers the basics of
+  graphics and FRP before Elm even had its record system!
 
- [infoq]: http://www.infoq.com/presentations/Elm "Elm at the Emerging Languages conference"
- [mlocjs]: http://www.ustream.tv/recorded/29330499 "Elm and the mloc.js conference"
+ [sl2013]: http://www.infoq.com/presentations/elm-reactive-programming
+ [mlocjs]: http://www.ustream.tv/recorded/29330499
+ [sl2012]: http://www.infoq.com/presentations/Elm
 
 #### Publications
 
-* [Concurrent FRP for GUIs][thesis] &ndash; overview of Elm and history of FRP
-* [Asynchronous FRP for GUIs][pldi] &ndash; formal semantics of Elm, PLDI 2013
+* [Concurrent FRP for GUIs][thesis] &mdash; overview of Elm and history of FRP
+* [Asynchronous FRP for GUIs][pldi] &mdash; formal semantics of Elm, PLDI 2013
 
  [thesis]: http://www.testblogpleaseignore.com/wp-content/uploads/2012/04/thesis.pdf "thesis"
  [pldi]: http://people.seas.harvard.edu/~chong/abstracts/CzaplickiC13.html "PLDI 2013 paper"
