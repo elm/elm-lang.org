ea7c0c3915a5b23d4928f81b7b53b7b9b88f6d52
Evan Czaplicki <info@elm-lang.org>
2014-02-24 17:36:08 -0500

    Give a slightly clearer name

--- public/Examples.elm
+++ public/Examples.elm
@@ -186,7 +186,7 @@ reactive = addFolder "Reactive/"
   , ("Random", [ ("Randomize", "Randomize") ])
   , ("Http",   [ ("Zip Codes", "ZipCodes") ])
   , ("Filters",[ ("Sample", "SampleOn")
-               , ("KeepIf", "KeepIf")
+               , ("Numbers Only", "KeepIf")
                ])
   , ("Ports",  [ ("Logging","Log")
                , ("Set Title","Title")
