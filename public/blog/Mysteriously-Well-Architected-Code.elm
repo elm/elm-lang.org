7cc4f97eef3b63e49b8b485f86d716ecd1612113
Evan Czaplicki <info@elm-lang.org>
2014-03-13 18:22:08 -0700

    Redo the intro, make the section on UI elements nicer

--- public/blog/Mysteriously-Well-Architected-Code.elm
+++ public/blog/Mysteriously-Well-Architected-Code.elm
@@ -1,35 +1,51 @@
 
-import Website.Skeleton (skeleton)
+import Website.Blog (skeleton)
 import Window
 import JavaScript as JS
 import Graphics.Input (Input, input, checkbox)
 
 port title : String
-port title = "Mysteriously well-architected code"
+port title = "Mysteriously Well-Architected Code"
 
-main = skeleton <~ lift everything check.signal ~ Window.dimensions
+main = skeleton <~ lift everything check.signal ~ Window.width
 
 check : Input Bool
 check = input False
 
 everything : Bool -> Int -> Element
 everything checked wid =
-    let w = min 600 wid
-        box = checkbox check.handle id checked
-    in  flow down
-        [ width w intro
-        , container w 50 middle <| flow right [ box, box, box ]
-        , width w rest
-        ]
+  let w  = truncate (toFloat wid * 0.8)
+      w' = min 600 w
+      section txt = let words = width w' txt
+                    in  container w (heightOf words) middle words
+      box = checkbox check.handle id checked
+  in  flow down
+      [ width w pageTitle
+      , section intro
+      , container w 50 middle <| flow right [ box, box, box ]
+      , section rest
+      ]
+
+pageTitle = [markdown|
+<br/>
+<div style="font-family: futura, 'century gothic', 'twentieth century', calibri, verdana, helvetica, arial; text-align: center;">
+<div style="font-size: 4em;">Mysteriously Well-Architected</div>
+<div style="font-size: 1.5em;">The structure of Elm programs and what it means for text input</div>
+</div>
+|]
 
 intro = [markdown|
 
 <style type="text/css">
-p { text-align: justify }
-pre { background-color: white;
-      padding: 10px;
-      border: 1px solid rgb(216, 221, 225);
-      border-radius: 4px;
+p, li {
+  text-align: justify;
+  line-height: 1.5em;
+}
+pre {
+  background-color: white;
+  padding: 10px;
+  border: 1px solid rgb(216, 221, 225);
+  border-radius: 4px;
 }
 code > span.kw { color: #268BD2; }
 code > span.dt { color: #268BD2; }
@@ -44,25 +60,27 @@ code > span.re { }
 code > span.er { color: #D30102; font-weight: bold; }
 </style>
 
-<h1><div style="text-align:center">Mysteriously well-architected code
-<div style="padding-top:4px;font-size:0.5em;font-weight:normal">*The structure of Elm programs and what it means for text input*</div></div>
-</h1>
-
-[The 0.12 release of Elm](/blog/announce/0.12.elm) introduced an entirely new
-API for user input. Creating traditional forms with buttons, text fields, and
-check boxes should finally be a pleasant experience in Elm. This post explains:
-
-  1. The thinking and motivation behind the API.
-  2. How to actually use the API in real life!
-
-## Mysteriously well-architected code
-
-I often find myself at the end of a messy hacking / prototyping session with
+<br/>I often find myself at the end of a messy hacking / prototyping session with
 Elm code that is mysteriously well-architected. I know I did not plan ahead or
 have a deep understanding of how the different parts of my program would
 eventually fit together, yet the input, model, update, and display are all
 neatly separated in my rough draft. Okay, but I designed the language so surely
-it is no surprise if my Elm code turns out nice, right?
+that explains it, right?
+
+It turns out that the core design choices behind FRP in Elm quitely guide you
+towards well-architected programs. Weirdly, this is something I have learned
+from Elm, not something I planned! This post is first going to describe the
+architecture that emerges from Elm programs. I think it is a great architecture
+for any GUI application, so the general idea can be useful in JavaScript,
+TypeScript, ClojureScript, and whatever else you are using to make UIs.
+
+From there we will dive into how this architecture guided the recent redesign of
+user input in [Elm 0.12](/blog/announce/0.12.elm). This overhaul has made UI
+elements like text fields and buttons significantly easier to use in Elm. I am
+really happy with how the new API came out and suspect the overarching design
+may be useful for other languages or GUI frameworks.
+
+## Mysteriously well-architected code
 
 I really took note of &ldquo;mysteriously well-architected code&rdquo; when I
 observed it during my week at [Hacker School](https://www.hackerschool.com/).
@@ -75,14 +93,14 @@ their program should work, and somehow their code always came out well-architect
 They all modelled the problem, created a way to update the model based on inputs,
 and had a section of code for display. All neatly separated!
 
-I knew that Elm's version of FRP strongly influenced the structure of Elm
+I knew that Elm's formulation of FRP strongly influenced the structure of Elm
 programs, but at this point I began to suspect that it actually *forces* people
 to write well-architected code. Regardless of skill level, experience, or
 knowledge of FRP, the code just comes out well-architected.
 
 ## How can this be?
 
-FRP in Elm is somewhat unique in that all signal graphs are static. This
+One key property of FRP in Elm is that all signal graphs are static. This
 is the same property that made it really simple to implement [hot-swapping in
 Elm](/blog/Interactive-Programming.elm). [My
 thesis](http://www.testblogpleaseignore.com/wp-content/uploads/2012/04/thesis.pdf)
@@ -91,24 +109,22 @@ are static&rdquo; means *exactly*, but I am about to explain the &ldquo;mysterio
 well-architected code&rdquo; in a way that does not require that kind of background.
 
 Due to the design of FRP in Elm, time-dependencies are always explicit *in your
-source code*. As a rough example, consider that position and velocity of
-[Mario](/edit/examples/Intermediate/Mario.elm) depends on time passing and
-arrow keys. In Elm, you say &ldquo;Mario is what you get when you combine time
-and key presses in this exact way&rdquo;. Describing Mario in Elm *requires*
-you to make the dependencies explicit! As a result, Elm programs always break
-into four distinct parts:
+source code*. Another way to say this is that the structure your application will
+manifest itself the structure of your code. As a rough example, consider that
+position and velocity of [Mario](/edit/examples/Intermediate/Mario.elm) depends
+on time passing and arrow keys. In Elm, you say &ldquo;Mario is what you get
+when you combine time and key presses in this exact way&rdquo;. Describing
+Mario *requires* you to make these dependencies explicit! As a result, Elm
+programs always break into four distinct parts:
 
-  1. Input &mdash; events from “the world”: mouse, touch, time, etc.
-
+  1. Input &mdash; events from “the world”. Mouse, touch, time, etc.
   2. Model &mdash; a full representation of the state in the program
-
   3. Update &mdash; functions for updating our model based on inputs
-
-  4. Display &mdash; functions describing the user’s view of the model
+  4. Display &mdash; functions to display the model to “the world”
 
 So at a very high level you can think of an Elm program as having this structure:
 
-<img src="/imud.png" style="display:block; margin:auto;" width="510" height="200">
+<img src="/imud.png" style="display:block; margin:auto;" width="600" height="240">
 
 The Elm runtime sends events from the world to your inputs, you use those events
 to update your model, you describe how that should get displayed, and the Elm runtime
@@ -118,25 +134,26 @@ anything you write will come out with this architecture!
 ## What's this got to do with UI?
 
 I bring this up specifically because UI elements like text fields and checkboxes
-seem to inherently ruin our nice separation of concerns. The traditional concept
-of a text field holds state and can trigger events. It *requires* that we have
+seem to inherently ruin everything I just said. The traditional concept of a
+text field holds state and can trigger events. It *requires* that we have
 inputs and model in our display! This is why the Controller in
 [Model-View-Controler][mvc] (MVC) can easily turn into a crazy tangled mess.
 
   [mvc]: http://en.wikipedia.org/wiki/Model%E2%80%93view%E2%80%93controller
 
-The primary goal of Elm 0.12 was to figure out how to solve this problem.
-An Elm program must cleanly separate input, model, update, and view, so **how
-can we create a library for UI elements that respects this structure *and* is
-pleasant to use?** The earlier version respected the structure of Elm programs,
-but no one could figure out how the hell it worked.
+The primary goal of [Elm 0.12](/blog/announce/0.12.elm) was to figure out how
+to solve this problem. An Elm program must cleanly separate input, model,
+update, and view, so **how can we create a library for UI elements that respects
+this structure *and* is pleasant to use?** The answer in previous versions of
+Elm respected the structure of Elm programs, but no one could figure out how
+the hell it worked.
 
 The new approach is based on the idea that **UI elements and inputs are
 separate things and should have separate representations**. A slightly more
 precise way to say this is that:
 
   * All inputs must be defined explicitly. If you have a bunch of checkboxes,
-    they will report to an input.
+    they will report to a separately defined input.
 
   * All functions to create UI elements are [pure functions][pure]. They display
     part of the model on screen and nothing more. If you want to change the
@@ -145,20 +162,9 @@ precise way to say this is that:
   [pure]: http://en.wikipedia.org/wiki/Pure_function
 
 Separating UI into these two distinct concepts lets us keep the nice structure
-of Elm programs. The rest of this post will be going into the particulars of
-how the API works and showing some examples of its use, but if you prefer to
-just read code you can jump right into it with some simple examples
-([text fields](/edit/examples/Reactive/TextField.elm),
-[passwords](/edit/examples/Reactive/Password.elm),
-[checkboxes](/edit/examples/Reactive/CheckBox.elm),
-[drop downs](/edit/examples/Reactive/DropDown.elm),
-[fields + HTTP](/edit/examples/Reactive/ZipCodes.elm),
-[filtered fields](/edit/examples/Reactive/KeepIf.elm))
-or some more advanced examples
-([field reversal](/edit/examples/Intermediate/TextReverse.elm),
-[calculator](/edit/examples/Intermediate/Calculator.elm),
-[sign-up form](/edit/examples/Intermediate/Form.elm),
-[todo list](https://github.com/evancz/TodoFRP)).
+of Elm programs. The rest of this post will be going into how the API works and
+showing some examples of its use. At the end of the post I will link to a ton of
+examples.
 
 ## Inputs
 
@@ -180,11 +186,52 @@ numbers = input 42
 The argument to `input` serves as the default value of the `Input`&rsquo;s
 `signal`, so the initial value of `numbers.signal` is 42.
 
+## UI Elements
 
+There are a bunch of UI elements available in [`Graphics.Input`][gi] and
+[`Graphics.Input.Field`][gif] but we will focus on checkboxes which do a
+great job illustrating the general pattern of UI elements in Elm. [The
+`checkbox` function][box] creates checkboxes for everyone to click on, and it
+has this type:
 
+  [gi]: http://library.elm-lang.org/catalog/evancz-Elm/0.12/Graphics-Input
+  [gif]: http://library.elm-lang.org/catalog/evancz-Elm/0.12/Graphics-Input-Field
+  [box]: http://library.elm-lang.org/catalog/evancz-Elm/0.11.2/Graphics-Input#checkbox
 
-To make this more
-concrete, let's see an example of synced checkboxes:
+```haskell
+checkbox : Handle a -> (Bool -> a) -> Bool -> Element
+checkbox handle processingFunction checked = ...
+```
+
+The three arguments work like this:
+
+  1. The first argument is a `Handle`. Clicking on a checkbox generates an event, and
+  the handle specifies which `Input` these events should be sent to. The generated
+  event is a boolean value that represents what the user *wants* the checkbox to
+  be. An unchecked box generates a `True` event, and a checked box generates a
+  `False` event.
+
+  2. The second argument of `checkbox` processes this event before sending it along
+  to the `Input` specified with the first argument. This processing function lets
+  us add extra information to the boolean value, such as an ID to indicate which
+  checkbox has been clicked. In simple cases, you usually do not need to add extra
+  information and can use `id` as your processing function.
+
+  3. The third argument is the actual state of the checkbox: whether it is checked or
+  not. This means `checkbox` is a pure function! Whether it is checked or not is an
+  argument, so that information must live in your model. The display is no more
+  than a data structure that we show on screen.
+
+This may not all be totally clear yet, but it should start to make more sense as
+we see how inputs and UI elements fit together. I think this is most clear when
+you see it in a diagram:
+
+<img src="/imud-computer.png" style="display:block; margin:auto;" width="460" height="510">
+
+The arrow coming from Display is the static image to show on screen. When you click on
+a button or type into a text field, Elm generates events that flow to the Input.
+
+## Two Small Examples
 
 |]
 
@@ -232,39 +279,45 @@ displayBoxes checked =
 In this example, we first create the `check` input for checkboxes to report to.
 We have given the `input` function `False`, so the default value of `check.signal`
 is `False`. In `main` we display the current value of `check.signal` with
-`displayBoxes`. This function displays three synced checkboxes using
-[`checkbox`][checkbox] from the overhauled [`Graphics.Input`][input] library.
-This is where we see the new API in action! The type of `checkbox` looks like
-this:
-
-  [checkbox]: http://library.elm-lang.org/catalog/evancz-Elm/0.11.2/Graphics-Input#checkbox
-  [input]: http://library.elm-lang.org/catalog/evancz-Elm/0.11.2/Graphics-Input
-
-```haskell
-checkbox : Handle a -> (Bool -> a) -> Bool -> Element
-```
-
-The first argument is a handle, specifying which `Input` to report to. In our
-example that is `check.handle`. Next we have a function that processes input before
-it is reported to `check.signal`. In our example, we use the `id` function to pass
-the new value along to the `check` input. Finally, we give the actual value of the
-checkbox, whether it is checked or not. So the result of `(displayBoxes True)`
-would show three checked boxes and `(displayBoxes False)` would show three
-unchecked ones.
-
-Notice that `checkbox` is a pure function! As a result, *no state is held in
-the display*. The display is simply a data structure that we show on screen,
-so we can keep the clean separation of input, model, update, and display:
-
-<img src="/imud-computer.png" style="display:block; margin:auto;" width="460" height="510">
-
-The arrow coming from Display is the static image to show on screen. When you click on
-a button or type into a text field, Elm generates events that flow to the Input.
-
-## Fancier Checkboxes
+`displayBoxes`.
 
 Synced checkboxes are nice for an introductory example, but it is simple specifically
 because the model and update are very trivial. This section will show another example
 that is a bit more complicated.
+
+## More Examples
+
+Start with the simple examples:
+
+  * [text fields](/edit/examples/Reactive/TextField.elm)
+  * [passwords](/edit/examples/Reactive/Password.elm)
+  * [checkboxes](/edit/examples/Reactive/CheckBox.elm)
+  * [drop downs](/edit/examples/Reactive/DropDown.elm)
+  * [fields + HTTP](/edit/examples/Reactive/ZipCodes.elm)
+  * [filtered fields](/edit/examples/Reactive/KeepIf.elm)
+
+And then take a look at some more advanced examples:
+
+  * [field reversal](/edit/examples/Intermediate/TextReverse.elm)
+  * [calculator](/edit/examples/Intermediate/Calculator.elm)
+  * [sign-up form](/edit/examples/Intermediate/Form.elm)
+  * [todo list](https://github.com/evancz/TodoFRP)
+
+These API is brand new, so I hope to see some libraries to spring up in the
+[Elm Public Library](library.elm-lang.org) to factor out common patterns like
+form-validation, password verification, credit-card entry, etc.
+
+<br>
+
+## Thank you!
+
+Huge thank you to [Spiros Eliopoulos](https://github.com/seliopou) whose work
+on [elm-d3](https://github.com/seliopou/elm-d3) led inspired the `signal` field
+of an `Input`. Another huge thank you to Jeff Smitts for talking through
+Spiros's idea with me and inspiring the `handle` field of an `Input`. Finally
+thanks to everyone on [the mailing list][list] for discussing and reviewing
+APIs, docs, and examples as the new UI elements library came together!
+
+  [list]: https://groups.google.com/forum/#!forum/elm-discuss
 
 |]
