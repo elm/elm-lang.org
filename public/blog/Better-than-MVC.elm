295ed4d120ca79b5fb0be8a86c9f2e89ac31ff47
Evan Czaplicki <info@elm-lang.org>
2014-03-16 14:05:46 -0700

    Use the idea that Elm gives you MVC without the C

--- public/blog/Better-than-MVC.elm
+++ public/blog/Better-than-MVC.elm
@@ -32,8 +32,8 @@ everything checked w =
 pageTitle = [markdown|
 <br/>
 <div style="font-family: futura, 'century gothic', 'twentieth century', calibri, verdana, helvetica, arial; text-align: center;">
-<div style="font-size: 4em;">Better than MVC</div>
-<div style="font-size: 1.5em;">How FRP in Elm improves upon MVC</div>
+<div style="font-size: 4em;">Stop wasting time with MVC</div>
+<div style="font-size: 1.5em;">How Elm takes the C out of MVC</div>
 </div>
 |]
 
@@ -64,61 +64,79 @@ code > span.er { color: #D30102; font-weight: bold; }
 </style>
 
 <br/>
-During my week as a resident at [Hacker School](https://www.hackerschool.com/)
-I saw a shocking amount of well-architected Elm code.
+I never have to write a Controller again. I realized this during my week as a
+resident at [Hacker School](https://www.hackerschool.com/) where I saw a
+shocking amount of well-architected Elm code.
 Students who had never programmed in *any* functional language before were picking
 up Elm and writing things like [Vessel](http://slawrence.github.io/vessel/).
 I was very impressed that they were able to accomplish so much in just one
 week, but the craziest part was that [the code](https://github.com/slawrence/vessel)
 always looked great! I mean, these students were very talented, but it did not quite make
-sense. These are students learning new syntax, how to understand
-[types](/learn/Getting-started-with-Types.elm) and type
-inference, figuring out a bunch of libraries, thinking of how their program should
-work, and somehow their code *always* came out well-architected.
+sense. These are students dealing with new [syntax](/learn/Syntax.elm), learning
+about [types](/learn/Getting-started-with-Types.elm) and type inference,
+figuring out [a bunch of libraries](http://library.elm-lang.org/catalog/evancz-Elm/0.12),
+thinking of [how their program should work](/blog/Pong.elm), and somehow their
+code *always* came out well-architected.
 
-It turns out that Elm's style of [Functional Reactive Programming](/learn/What-is-FRP.elm)
-(FRP) strongly influences the structure of Elm programs. In fact, it *forces*
-you to write well-architected code. The most interesting thing is that this
-architecture is a fairly substantial improvement over the traditional
-[Model-View-Controller][mvc] (MVC) approach. FRP in Elm blah blah blah.
+It turns out that well-architected programs are a direct consequence of Elm's
+style of [Functional Reactive Programming][frp] (FRP). FRP
+in Elm quietly and reliably guides you to great architecture that is a
+substantial improvement over the traditional [Model-View-Controller][mvc] (MVC)
+approach. And it is not just that Elm forces you to cleanly separate your Model
+and View, **with Elm you never need to write a Controller again**.
 
+  [frp]: /learn/What-is-FRP.elm
   [mvc]: http://en.wikipedia.org/wiki/Model%E2%80%93view%E2%80%93controller
 
 ## A better way than MVC
 
-At a very high level, all Elm program have the following architecture:
+Mutation is at the core of MVC, particularly in the controller. The controller
+usually becomes a large tangle of callbacks and DOM manipulation. It is a common
+source of bugs and inefficiencies. The root of the problem is that it forces you
+to *manually* manage a complex set of events flowing between the model and view.
+
+Managing complex events and time-dependencies is what [FRP][frp] is all about!
+Elm's runtime system is essentially a general purpose Controller. It takes care
+of efficiently updating the DOM in batches with `requestAnimationFrame` and
+keeping the model and view synced. By taking care of the controller, there is
+a whole class of problems and bugs you just do not have to deal with in Elm.
+You can focus on the *meaningful* parts of your application.
+
+FRP in Elm also guides you towards a very particular architecture. At a very
+high level, *all* Elm program look like this:
 
 <img src="/imud.png" style="display:block; margin:auto;" width="600" height="240">
 
-An **Input** is a stream of events coming into your program. Inputs are things
-like mouse position, key presses, touches, button presses, etc. Any events you
-need to describe how your program works. Notice that inputs are completely
-separate from the Display! In Elm, you must make all inputs explicit.
+ * An **Input** is a stream of events coming into your program. Inputs are things
+   like mouse position, key presses, touches, button presses, etc. Any events you
+   need to describe how your program works. Notice that inputs are completely
+   separate from the Display! In Elm, you must make all inputs explicit.
 
-The **Model** is a full representation of the current state of the program.
-It is an immutable data structure&mdash;often a [record](/learn/Records.elm)&mdash;that
-holds all information relevant to your application. There is no cheating with
-your model in Elm. If there is information you need, you cannot sneak it into
-your display!
+ * The **Model** is a full representation of the current state of the program.
+   It is an immutable data structure&mdash;often a [record](/learn/Records.elm)&mdash;that
+   holds all information relevant to your application. There is no cheating with
+   your model in Elm. If there is information you need, you cannot sneak it into
+   your display!
 
-The **Update** is a way to update your Model as new Input comes in. In Elm
-it is just a collection of [pure functions](http://en.wikipedia.org/wiki/Pure_function)
-that transform your model. As is typical in functional languages, this tends to
-break up into [small, orthogonal functions](/edit/examples/Intermediate/Mario.elm).
-This means the Update code is very easy to unit test or load up into a REPL and
-play with.
+ * The **Update** is a way to update your Model as new Input comes in. In Elm
+   it is just a collection of [pure functions](http://en.wikipedia.org/wiki/Pure_function)
+   that transform your model. As is typical in functional languages, this tends to
+   break up into [small, orthogonal functions](/edit/examples/Intermediate/Mario.elm).
+   This means the Update code is very easy to unit test or load up into a REPL and
+   play with.
 
-The **Display** is a set of pure functions that convert a Model into something
-you can show on screen. The display code *only* cares about the model:
-&ldquo;how do I show this information on screen?&rdquo;
+ * The **Display** is a set of pure functions that convert a Model into something
+   you can show on screen. The display code *only* cares about the model:
+   &ldquo;how do I show this information on screen?&rdquo;
 
 These four parts give you a very clear separation of concerns. Perhaps the most
 interesting thing about this architecture is that it *emerges* from Elm's
 formulation of FRP. This was not something I set out to design, but rather the
-inevitable result of purity, immutability, and Elm's style of FRP.
+inevitable result of purity, immutability, and Elm's style of FRP. This means
+even the messiest of hacking and prototyping sessions will have this architecture.
+So if you write it in Elm, you get well-architected code every time.
 
-<span style="color:grey;">
-Technical Note: A key property of FRP in Elm is that
+<span style="opacity:0.4;">Technical Note: A key property of FRP in Elm is that
 all signal graphs are static. This property is not very common in FRP systems,
 but it is the core reason that Elm code comes out well-architected every time.
 This property also makes it [really simple to implement
@@ -127,24 +145,17 @@ thesis](http://www.testblogpleaseignore.com/wp-content/uploads/2012/04/thesis.pd
 is a pretty accessible way to dig into this in more detail.
 </span>
 
+## How does this work with UI elements?
 
-
-## What's this got to do with UI?
-
-I bring this up specifically because UI elements like text fields and checkboxes
-seem to inherently ruin everything I just said. The traditional concept of a
-text field holds state and can trigger events. It *requires* that we have
-inputs and model in our display! This is why the Controller in
-[Model-View-Controler][mvc] (MVC) can easily turn into a crazy tangled mess.
-
-  [mvc]: http://en.wikipedia.org/wiki/Model%E2%80%93view%E2%80%93controller
+UI elements like text fields and checkboxes seem to inherently ruin everything
+I just said. The traditional concept of a text field holds state and can trigger
+events. It seems to *require* that we have Inputs and Model in our Display! This
+is one common reason the Controller in MVC can easily turn into a crazy tangled mess.
 
 The primary goal of [Elm 0.12](/blog/announce/0.12.elm) was to figure out how
 to solve this problem. An Elm program must cleanly separate input, model,
 update, and view, so **how can we create a library for UI elements that respects
-this structure *and* is pleasant to use?** The answer in previous versions of
-Elm respected the structure of Elm programs, but no one could figure out how
-the hell it worked.
+this structure *and* is pleasant to use?**
 
 The new approach is based on the idea that **UI elements and inputs are
 separate things and should have separate representations**. A slightly more
@@ -173,13 +184,15 @@ way to for the display to communicate with the input:
 <img src="/imud-computer.png" style="display:block; margin:auto;" width="460" height="450">
 
 The arrow from Display to the monitor indicates that Elm's runtime is
-taking an `Element` from your program and showing it on screen. This happens in
-every Elm program. The arrow from the monitor to the Input is the special part.
-When you click on a button or type into a text field, the Elm runtime generates
-events that flow to a particular Input. The key trick is to explicitly
-define the input, the UI element, and crucially *a way for UI elements to refer
-to specific inputs*. The next two subsections describe how these three parts
-fit together.
+taking an `Element` from your program and showing it on screen. With MVC you
+would be manually mutating the view in your controller.
+
+The arrow from the monitor to the Input is the special part. When you click on
+a button or type into a text field, the Elm runtime generates events that flow
+to a particular Input. Again, with MVC you would be doing this manually in the
+controller. The key trick to make it possible for the Elm runtime to fully
+manage this is to explicitly define Inputs. The next two subsections
+describe how this works in detail.
 
 ### Inputs
 
@@ -191,10 +204,9 @@ type Input a = { signal : Signal a, handle : Handle a }
 
 An `Input` has two distinct parts. One is a `signal` of events coming from UI
 elements. This signal will be used to update our model. The more subtle part
-of an `Input` is the `handle`. This is simply a way for UI elements to refer to
-a specific input. This is the arrow from the monitor to the Input. It tells the
-Elm runtime where to send events when the user clicks on a check box or a drop
-down menu.
+of an `Input` is the `handle`. This is how UI elements refer to a specific input.
+This is the arrow from the monitor to the Input. It tells the Elm runtime where
+to send events when the user clicks on a check box or a drop down menu.
 
 You create an `Input` with the `input` function, like this:
 
@@ -253,7 +265,7 @@ Breaking the concept of a checkbox into an `Input`, a `Handle`, and a `checkbox`
 makes the flow of events very explicit. I think these functions become much
 clearer when you actually see them used!
 
-## Two Small Examples
+## So Many Examples
 
 Our first example is a set of three synced checkboxes. Changing one of them
 will change the two others:
@@ -316,48 +328,29 @@ Finally, in `main` we display the current value of `check.signal` with
 
 Synced checkboxes are nice for an introductory example, but it is simple because
 it really only has an input and a display section. There is not a lot to model
-or update. In the next example we will have three checkboxes that change
-independently. This means we will need to model the state of these boxes and
-write code to update the model when new events come in.
+or update. I have created a bunch of examples to illustrate some more complex
+situations. To see this in action start with the simple examples:
 
-```haskell
-import Input (Input, input, checkbox)
-
-check : Input (Int, Bool)
-check = input False
-
-displayBoxes : Bool -> Element
-displayBoxes isChecked =
-    let box = checkbox check.handle id isChecked
-    in  flow right [ box, box, box ]
-
-main : Signal Element
-main = displayBoxes <~ check.signal
-```
-
-## More Examples
-
-Start with the simple examples:
-
-  * [text fields](/edit/examples/Reactive/TextField.elm)
-  * [passwords](/edit/examples/Reactive/Password.elm)
-  * [checkboxes](/edit/examples/Reactive/CheckBox.elm)
-  * [drop downs](/edit/examples/Reactive/DropDown.elm)
-  * [fields + HTTP](/edit/examples/Reactive/ZipCodes.elm)
-  * [filtered fields](/edit/examples/Reactive/KeepIf.elm)
+  * [Text Fields](/edit/examples/Reactive/TextField.elm)
+  * [Passwords](/edit/examples/Reactive/Password.elm)
+  * [Checkboxes](/edit/examples/Reactive/CheckBox.elm)
+  * [Drop Downs](/edit/examples/Reactive/DropDown.elm)
+  * [Fields + HTTP](/edit/examples/Reactive/ZipCodes.elm)
+  * [Filtered Fields](/edit/examples/Reactive/KeepIf.elm)
 
 And then take a look at some more advanced examples:
 
-  * [field reversal](/edit/examples/Intermediate/TextReverse.elm)
-  * [calculator](/edit/examples/Intermediate/Calculator.elm)
-  * [sign-up form](/edit/examples/Intermediate/Form.elm)
-  * [todo list](https://github.com/evancz/TodoFRP)
+  * [Field Reversal](/edit/examples/Intermediate/TextReverse.elm)
+  * [Calculator](/edit/examples/Intermediate/Calculator.elm)
+  * [Sign-up Form](/edit/examples/Intermediate/Form.elm)
+  * [Todo List](https://github.com/evancz/TodoFRP)
 
-These API is brand new, so I hope to see some libraries to spring up in the
-[Elm Public Library](library.elm-lang.org) to factor out common patterns like
-form-validation, password verification, credit-card entry, etc.
+The APIs for UI elements are brand new, so I hope to see some libraries to
+spring up in the [Elm Public Library](library.elm-lang.org) to factor out
+common patterns like form-validation, password verification, credit-card
+entry, etc.
 
-<br>
+<br/>
 
 ## Thank you!
 
