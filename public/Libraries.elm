892eba2e21d87c7b878cc21f2b0a239b69416e3f
Evan Czaplicki <info@elm-lang.org>
2014-03-27 15:14:11 -0700

    Link directly to the library website on the Libraries page

--- public/Libraries.elm
+++ public/Libraries.elm
@@ -1,95 +1,40 @@
+import Website.Button as B
 import Website.Skeleton (skeleton)
 import String
 import Window
 
-general = ("General",
-  [ "Basics"
-  , "String"
-  , "Regex"
-  , "Char"
-  , "Date"
-  , "Bitwise"
-  , "Trampoline"
-  , "Debug"
-  ])
-containers = ("Containers",
-  [ "List"
-  , "Dict"
-  , "Set"
-  , "Maybe"
-  , "Either"
-  ])
+main = lift (skeleton content) Window.dimensions
 
-graphics = ("Graphics",
-  [ "Graphics.Element"
-  , "Graphics.Collage"
-  , "Graphics.Input"
-  , "Graphics.Input.Field"
-  , "Color"
-  , "Text"
-  , "Transform2D"
-  ])
-
-signals = ("Interaction",
-  [ "Signal"
-  ])
-userInput = ("User Input",
-  [ "Mouse"
-  , "Keyboard"
-  , "Touch"
-  ])
-systemInput = ("System Input",
-  [ "Window"
-  , "Time"
-  , "Random"
-  , "Http"
-  , "WebSocket"
-  ])
-ffi = ("JavaScript",
-  [ "JavaScript"
-  , "Json"
-  , "JavaScript.Experimental"
-  ])
+content w =
+  flow down
+  [ width w intro
+  , B.button w 400 "http://library.elm-lang.org/catalog/evancz-Elm/0.12/" "Standard Libraries"
+  , width w midtro
+  , B.button w 400 "http://library.elm-lang.org/catalog" "Community Libraries"
+  , width w outro
+  ]
 
 intro = [markdown|
 
 # Libraries
 
-The Standard Libraries, listed below, come with the latest release of the Elm
-compiler and make it easy to get productive with Elm. Search for
-functions like `map` or `fold` in the standard library
-[by filtering](http://library.elm-lang.org/catalog/evancz-Elm/0.12/).
-
-See the [syntax reference](/learn/Syntax.elm) and [other learning
-resources](/Learn.elm) to learn more about the language itself.
-Check out [library.elm-lang.org](http://library.elm-lang.org) to discover
-3rd party libraries and browse their documentation.
+The Standard Libraries come with the latest release of the Elm compiler and
+make it easy to get productive. When you need documentation for functions like
+`map` and `length` or operators like `/=` and `|>`, just **filter** to find out
+which module it is in:
 
 |]
 
-deslash c = if c == '.' then '-' else c
+midtro = [markdown|
 
-linkify name =
-    let path = "http://library.elm-lang.org/catalog/evancz-Elm/0.12/" ++ String.map deslash name
-    in  Text.toText "  " ++ Text.link path (Text.toText name)
+If you cannot find it in the Standard Libraries, the Elm community is probably
+working on it already. Browse community libraries and check out their documentation:
 
-linkList (name, pairs) = 
-  flow down . intersperse (spacer 2 4) . map Text.leftAligned <|
-  Text.height 18 (Text.toText name) :: map linkify pairs
+|]
 
-makeCol w = width w . flow down . intersperse (spacer 10 20) . map linkList
-threeCol w l m r =
-    flow right <| map (makeCol (min 220 <| w `div` 3)) [l,m,r]
+outro = [markdown|
 
-col1 = [ general, containers ]
-col2 = [ signals, userInput, systemInput ]
-col3 = [ graphics, ffi ]
+See the [syntax reference](/learn/Syntax.elm) and [other learning
+resources](/Learn.elm) to learn more about the language itself.
 
-content w =
-  flow down
-  [ width w intro
-  , spacer w 20
-  , threeCol w col1 col2 col3
-  ]
-
-main = lift (skeleton content) Window.dimensions
+|]
