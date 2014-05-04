eeb988d8223e3ffa9cbef2335545c9b9777f4f63
Evan Czaplicki <info@elm-lang.org>
2014-05-04 10:19:06 -0700

    hsv => hsl

--- public/examples/Intermediate/Circles.elm
+++ public/examples/Intermediate/Circles.elm
@@ -1,11 +1,14 @@
 
-makePoint radius angle =
-    circle 5 |> filled (hsv (degrees radius) 1 1)
-             |> move (radius * cos angle, radius * sin angle)
+main : Element
+main = collage 300 300 (concatMap makeLevel [1..6])
 
+makeLevel : Float -> [Form]
 makeLevel level =
   let n = 6 * level
       pointAt i = makePoint (level * 14) (turns (i/n))
   in  map pointAt [0..n]
 
-main = collage 300 300 (concatMap makeLevel [1..6])
+makePoint : Float -> Float -> Form
+makePoint radius angle =
+    circle 5 |> filled (hsl (degrees radius) 0.8 0.6)
+             |> move (radius * cos angle, radius * sin angle)
