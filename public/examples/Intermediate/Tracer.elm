1ee34b6962d06aecabca4120588625726bc55427
Evan Czaplicki <info@elm-lang.org>
2014-05-22 15:53:21 -0700

    Start using zipN when possible, add some types

--- public/examples/Intermediate/Tracer.elm
+++ public/examples/Intermediate/Tracer.elm
@@ -58,21 +58,6 @@ stateSignal = foldp (\a b -> b + 1) 0 <| every <| rate*millisecond
 main = drawing <~ stateSignal
 
 
-
--- Helper functions
-
-zipWith4 : (a -> b -> c -> d -> e) -> [a] -> [b] -> [c] -> [d] -> [e]
-zipWith4 f ws xs ys zs = 
-  case (ws,xs,ys,zs) of
-    (w::ws, x::xs, y::ys, z::zs) -> f w x y z :: zipWith4 f ws xs ys zs
-    _ -> []
-
-zipWith3 : (a -> b -> c -> d) -> [a] -> [b] -> [c] -> [d]
-zipWith3 f xs ys zs = 
-  case (xs,ys,zs) of
-    (x::xs, y::ys, z::zs) -> f x y z :: zipWith3 f xs ys zs
-    _ -> []
-
 -- Create a list that starts with x and adjusts n times
 adjust x by n = case n of
   0 -> []
