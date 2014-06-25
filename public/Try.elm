50a497763d916f4776636ff3a453fc66582b8a95
Evan Czaplicki <info@elm-lang.org>
2014-06-25 15:37:07 +0200

    Use @mgold's suggestion for better examples

--- public/Try.elm
+++ public/Try.elm
@@ -7,8 +7,8 @@ main = report [markdown|
 Write and compile code online!
 
   * <a href="/edit/examples/Elements/HelloWorld.elm" target="_top">Hello World!</a>
-  * <a href="/edit/examples/Elements/Image.elm" target="_top">Images</a>
-  * <a href="/edit/examples/Elements/LinearGradient.elm" target="_top">Gradients</a>
+  * <a href="/edit/examples/Reactive/Position.elm" target="_top">Mouse</a>
+  * <a href="/edit/examples/Intermediate/Clock.elm" target="_top">Clock</a>
 
 For more guidance check out:
 
