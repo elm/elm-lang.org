1db8283d94014161b2576862c8e4e65ef51c771e
Evan Czaplicki <info@elm-lang.org>
2014-08-26 12:58:16 +0200

    Add syntax for importing ADTs

--- public/learn/Syntax.elm
+++ public/learn/Syntax.elm
@@ -317,12 +317,16 @@ and [here](http://library.elm-lang.org/catalog/elm-lang-Elm/latest/Signal).
 module MyModule where
 
 -- qualified imports
-import List               -- List.map, List.foldl
-import List as L          -- L.map, L.foldl
+import List                    -- List.map, List.foldl
+import List as L               -- L.map, L.foldl
 
 -- open imports
-import List (..)          -- map, foldl, concat, ...
-import List (map,foldl)   -- map, foldl
+import List (..)               -- map, foldl, concat, ...
+import List ( map, foldl )     -- map, foldl
+
+import Maybe ( Maybe )         -- Maybe
+import Maybe ( Maybe(..) )     -- Maybe, Just, Nothing
+import Maybe ( Maybe(Just) )   -- Maybe, Just
 ```
 
 Qualified imports are preferred. Module names must match their file name,
