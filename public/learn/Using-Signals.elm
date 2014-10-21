95ce2d08a827f0921a85fd692e97793edc7cf879
Evan Czaplicki <info@elm-lang.org>
2014-10-20 18:21:08 -0700

    Add a basic introduction

--- public/learn/Using-Signals.elm
+++ public/learn/Using-Signals.elm
@@ -11,7 +11,14 @@ content w = width w [markdown|
 
 # Using Signals
 
-Coming soon!
+Signals are values that change over time. You can learn more about the basics
+of signals in [this post][frp] and in [the examples](/Examples.elm). This post
+focuses on common design patterns that you will definitely see when programming
+with signals in Elm.
+
+[frp]: /learn/What-is-FRP.elm
+
+To be continued!
 
 |]
 
