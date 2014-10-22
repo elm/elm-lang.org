653a3ebf212910e29e521d4f6366a701274e689b
Evan Czaplicki <info@elm-lang.org>
2014-10-22 02:46:23 -0700

    Write up "The Typical Pattern"

--- public/learn/Using-Signals.elm
+++ public/learn/Using-Signals.elm
@@ -18,7 +18,53 @@ with signals in Elm.
 
 [frp]: /learn/What-is-FRP.elm
 
-To be continued!
+## The Typical Pattern
+
+When writing Elm code, it is usually best to use signals as little as possible.
+They help you handle inputs from the world and manage state, but when it comes
+to writing nice modular code, you should primarily use normal functions and
+values.
+
+Okay, but what about the part that *does* use signals?
+
+The state of your application will live primarily in a single [`foldp`][foldp].
+This `foldp` will take in some signal of &ldquo;inputs&rdquo; that indicate
+how your application state should be updated.
+
+[foldp]: http://library.elm-lang.org/catalog/elm-lang-Elm/latest/Signal#foldp
+
+Say those inputs include `Mouse.clicks` and time deltas. We need to put these
+signals together in a way that will update our `foldp` correctly. It may seem
+tempting to write something like this:
+
+```haskell
+inputs : Signal ((), Float)
+inputs =
+    lift2 (,) Mouse.clicks (fps 40)
+```
+
+The `inputs` signal will now update whenever `Mouse.clicks` or `(fps 40)`
+update. That means we cannot know who triggered the update. Should I react to a
+click or a time delta?!
+
+Instead you want to model the kinds of updates that can be done and
+[`merge`][merge] them all together.
+
+[merge]: http://library.elm-lang.org/catalog/elm-lang-Elm/latest/Signal#merge
+
+```haskell
+type Update = Click | TimeDelta Float
+
+inputs : Signal Update
+inputs =
+    merge
+        (lift (always Click) Mouse.clicks)
+        (lift TimeDelta (fps 40))
+```
+
+When writing large applications, use the techniques described
+[here](/learn/Architecture.elm) to make this basic approach modular as your
+codebase grows.
 
 |]
 
