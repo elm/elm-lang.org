b83ba572ec131ebe68f568df68ec1b0a02f5602a
Evan Czaplicki <info@elm-lang.org>
2014-12-01 17:37:03 -0800

    Refactor, separate compile and generation, update for elm-make

---
+++ backend/Compile.hs
@@ -0,0 +1,54 @@
+module Compile (toJS) where
+
+import Control.Applicative ((<$>))
+import Control.Exception (catch, SomeException)
+import Control.Monad.Error (runErrorT)
+import System.Directory (removeFile)
+import System.FilePath ((</>), (<.>), dropExtension, replaceExtension)
+import System.IO (hClose, hPutStr, openTempFile)
+
+import qualified Elm.Utils as Utils
+
+
+toJS :: String -> IO (Either String (String, String))
+toJS source =
+  catchCrashes (compileSource source)
+
+
+compileSource :: String -> IO (Either String (String, String))
+compileSource source =
+  do  (elmFilePath, handle) <- openTempFile "." "Temp.elm"
+      let moduleName = dropExtension elmFilePath
+      hPutStr handle ("module " ++ moduleName ++ " where\n" ++ source)
+      hClose handle
+
+      let jsFilePath = replaceExtension elmFilePath "js"
+
+      compilerResult <-
+        runErrorT $
+            Utils.run "elm-make" [ elmFilePath, "--output=" ++ jsFilePath ]
+
+      result <-
+        case compilerResult of
+          Right _ ->
+            do  jsSource <- readFile jsFilePath
+                return (Right (moduleName, jsSource))
+
+          Left msg ->
+            return (Left msg)
+
+      removeFile elmFilePath
+      removeFile jsFilePath
+      removeFile ("elm-stuff" </> "build-artifacts" </> moduleName <.> "elmi")
+      removeFile ("elm-stuff" </> "build-artifacts" </> moduleName <.> "elmo")
+
+      return result
+
+
+catchCrashes :: IO (Either String a) -> IO (Either String a)
+catchCrashes io =
+    io `catch` recover
+  where
+    recover :: SomeException -> IO (Either String b)
+    recover exception =
+        return (Left (show exception))
--- backend/Generate.hs
+++ backend/Generate.hs
@@ -1,63 +1,57 @@
 {-# OPTIONS_GHC -Wall #-}
 {-# LANGUAGE OverloadedStrings #-}
-module Generate (html, js) where
+module Generate (htmlSkeleton, html, js) where
 
-import Control.Exception (evaluate, catches, Handler(..), SomeException)
-import Control.Monad    (forM_, when)
-import Data.Functor     ((<$>))
-import Data.Maybe       (fromMaybe)
-import System.Directory
-import System.Exit
-import System.FilePath
-import System.IO (openTempFile, hPutStr, hClose)
-import System.Process
-import Text.Blaze       (preEscapedToMarkup)
+import Control.Monad (forM_, when)
+import Text.Blaze (preEscapedToMarkup)
 import Text.Blaze.Html5 ((!))
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
 
-import qualified Elm.Internal.Utils as Elm
-import Elm.Internal.Version (elmVersion)
-import Utils
+import Compile
+import qualified Utils
 
 
--- | Using a page title and the full source of an Elm program, compile down to
---   a valid HTML document.
+-- CREATE HTML DOCUMENTS
+
+htmlSkeleton :: Bool -> String -> H.Html -> H.Html
+htmlSkeleton userGenerated title scripts =
+  H.docTypeHtml $ do 
+    H.head $ do
+      H.meta ! A.charset "UTF-8"
+      H.title (H.toHtml title)
+      H.style $ preEscapedToMarkup Utils.standardStyle
+      when (not userGenerated) $
+        do  H.link ! A.rel "stylesheet" ! A.href "/assets/highlight/styles/default.css"
+            H.script ! A.src "/assets/highlight/highlight.pack.js" $ ""
+
+    H.body scripts
+
+    when (not userGenerated) Utils.googleAnalytics
+
+
+-- USER-GENERATED ELM TO HTML
+
 html :: String -> String -> IO H.Html
-html name src =
-  do  compilerResult <- safeCompile src
-      return . buildPage $ formatResult compilerResult
-  where
-    script = H.script ! A.type_ "text/javascript"
+html title source =
+  do  compilerResult <- Compile.toJS source
+      let htmlChunk = resultToHtml compilerResult
+      return (htmlSkeleton True title htmlChunk)
 
-    formatResult compilerResult =
-        case compilerResult of
-          Right jsSrc ->
-              do script $ preEscapedToMarkup jsSrc
-                 script $ preEscapedToMarkup $ runFullscreen
-          Left err ->
-              H.span ! A.style "font-family: monospace;" $
-               forM_ (lines err) $ \line ->
-                   do preEscapedToMarkup (addSpaces line)
-                      H.br
 
-    runFullscreen =
-        let moduleName = "Elm." ++ fromMaybe "Main" (Elm.moduleName src)
-        in  "var runningElmModule = Elm.fullscreen(" ++  moduleName ++ ")"
+resultToHtml :: (Either String (String, String)) -> H.Html
+resultToHtml compilerResult =
+  case compilerResult of
+    Right (moduleName, jsSource) ->
+      do  H.script $ preEscapedToMarkup jsSource
+          H.script $ preEscapedToMarkup $
+            "var runningElmModule = Elm.fullscreen(Elm." ++  moduleName ++ ")"
 
-    buildPage content = H.docTypeHtml $ do
-        H.head $ do
-          H.meta ! A.charset "UTF-8"
-          H.title . H.toHtml $ name
-          H.style ! A.type_ "text/css" $ preEscapedToMarkup
-              ("a:link {text-decoration: none; color: rgb(15,102,230);}\n\
-               \a:visited {text-decoration: none; color: rgb(15,102,230);}\n\
-               \a:active {text-decoration: none}\n\
-               \a:hover {text-decoration: underline; color: rgb(234,21,122);}" :: String)
-        H.body $ do
-          script ! A.src (H.toValue ("/elm-runtime.js?" ++ show elmVersion)) $ ""
-          content
-        googleAnalytics
+    Left err ->
+      H.span ! A.style "font-family: monospace;" $
+         forM_ (lines err) $ \line ->
+             do  preEscapedToMarkup (addSpaces line)
+                 H.br
 
 
 addSpaces :: String -> String
@@ -68,76 +62,14 @@ addSpaces str =
     [] -> []
 
 
+-- USER-GENERATED ELM TO JS
+
 js :: String -> IO String
-js src =
-  do output <- safeCompile src
-     return (either (wrap "error") (wrap "success") output)
-  where
-    wrap :: String -> String -> String
-    wrap typ msg = "{ " ++ show typ ++ " : " ++ show msg ++ " }"
+js source =
+  do  result <- Compile.toJS source
+      case result of
+        Left msg ->
+          return $ "{ \"error\": " ++ show msg ++ " }"
 
-
-safeCompile :: String -> IO (Either String String)
-safeCompile src =
-  case Elm.nameAndImports src of
-    Nothing -> compileNormal src
-    Just (_name, imports) ->
-        if any (`elem` thirdPartyLibraries) imports
-          then compileInSandbox src
-          else compileNormal src
-
-
-thirdPartyLibraries :: [String]
-thirdPartyLibraries =
-    [ "Graphics.WebGL"
-    , "Math.Matrix4"
-    , "Math.Vector2"
-    , "Math.Vector3"
-    , "Math.Vector4"
-    ]
-
-compileNormal :: String -> IO (Either String String)
-compileNormal src =
-  do output <- catchBugs (Elm.compile src)
-     return (either explain id output)
-  where
-    explain problem = Left ("Error:\n" ++ problem)
-
-catchBugs :: a -> IO (Either String a)
-catchBugs inp = (Right <$> evaluate inp) `catches` handlers
-  where
-    handlers = [ Handler (return . Left . show :: SomeException -> IO (Either String b)) ]
-
-compileInSandbox :: String -> IO (Either String String)
-compileInSandbox src =
-  do (file, handle) <- openTempFile "." "Temp.elm"
-     hPutStr handle src
-     hClose handle
-     (exitCode, stdout, stderr) <- readProcessWithExitCode "elm" (args file) ""
-     case exitCode of
-       ExitFailure _ ->
-           do removeEverything file
-              return (Left (stdout ++ stderr))
-       ExitSuccess ->
-           do result <- readFile ("build" </> file `replaceExtension` "js")
-              length result `seq` removeEverything file
-              return (Right result)
-  where
-    args file =
-        [ "--make"
-        , "--only-js"
-        , file
-        ]
-
-    removeEverything :: FilePath -> IO ()
-    removeEverything file =
-        do remove "." "elm"
-           remove "cache" "elmi"
-           remove "cache" "elmo"
-           remove "build" "js"
-        where
-          remove :: String -> String -> IO ()
-          remove dir ext = do
-            let path = dir </> file `replaceExtension` ext
-            exists <- doesFileExist path
-            when exists (removeFile path)
+        Right (_, jsSource) ->
+          return $ "{ \"success\": " ++ show jsSource ++ " }"
