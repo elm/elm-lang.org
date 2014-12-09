4f1e13d0521616e2a5e892dae6d639546544b5da
Evan Czaplicki <info@elm-lang.org>
2014-12-08 21:46:49 -0800

    Add back markdown example, point to package

---
+++ frontend/public/examples/Elements/Markdown.elm
@@ -0,0 +1,14 @@
+import Markdown
+
+
+main = Markdown.toElement """
+
+# Welcome to Elm
+
+This [markdown](http://daringfireball.net/projects/markdown/)
+parser lives in the [elm-markdown][] package, allowing you to
+generate `Element` and `Html` blocks.
+
+[elm-markdown]: http://package.elm-lang.org/packages/evancz/elm-markdown/latest
+
+"""
