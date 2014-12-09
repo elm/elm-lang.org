be6f023a17450efd80f2cc140036dd0d4a864019
Evan Czaplicki <info@elm-lang.org>
2014-12-08 20:47:54 -0800

    shoutout to @rtfeldman

--- frontend/public/blog/announce/0.14.elm
+++ frontend/public/blog/announce/0.14.elm
@@ -243,7 +243,11 @@ viewButton id =
 
 It reads much more clearly now, hopefully making it easier to pick up. It also
 has some nice conceptual connections to my [thesis][], so the door is open for
-some cool stuff farther down the line!
+some cool stuff farther down the line! Huge thanks to [Richard
+Feldman](https://github.com/rtfeldman/dreamwriter) for talking through this
+until we ended up with this API! Your insights from writing
+[dreamwriter](https://github.com/rtfeldman/dreamwriter#dreamwriter) in Elm
+have been extremely valuable in general.
 
 
 ## Making JSON Easier
