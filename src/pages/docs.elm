7e59259736594a80a3263ae93a2cad76478868bd
Evan Czaplicki <info@elm-lang.org>
2015-03-30 19:25:08 -0700

    Show the outline of the guide

--- src/pages/docs.elm
+++ src/pages/docs.elm
@@ -1,9 +1,26 @@
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import String
 
 import TopBar
+import Outline
 
 
 main =
-  div [] [ TopBar.topBar "docs" ]
+  div []
+    [ TopBar.topBar "docs"
+    , ul [] (List.map viewChapter Outline.outline)
+    ]
 
+
+viewChapter : (String, List String) -> Html
+viewChapter (title, sections) =
+  li []
+    [ a [href ("/guide/" ++ format title)] [text title]
+    , ul []
+        (List.map (\name -> li [] [ text name ]) sections)
+    ]
+
+format str =
+  String.toLower str
+    |> String.map (\c -> if c == ' ' then '-' else c)
