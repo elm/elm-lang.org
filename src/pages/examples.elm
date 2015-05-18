40b145cea8dd6d98d797717e422766cd21d11149
Evan Czaplicki <info@elm-lang.org>
2015-05-18 15:57:42 +0200

    Add a bit of content to the examples page

--- src/pages/examples.elm
+++ src/pages/examples.elm
@@ -1,9 +1,34 @@
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
+import Center
 import TopBar
 
 
 main =
-  div [] [ TopBar.topBar "examples" ]
+  div []
+    [ TopBar.topBar "examples"
+    , Center.markdown "600px" content
+    ]
 
+
+content = """
+
+# Learn by Example
+
+Walk through a sequence of small examples, building skills one at a time by
+reading and modifying Elm code in the [online editor](/try).
+
+Remember to check the [syntax reference](/docs/syntax) and [docs](/docs) when
+you see new syntax or features!
+
+"""
+
+
+function =
+  [ "apply"
+  , "forward-apply"
+  , "composition"
+  , "anonymous"
+  , "infix"
+  ]
