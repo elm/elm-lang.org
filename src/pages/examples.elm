652ec6e4258a1acd1d1fdd0fd0a5160d6e36f5f6
Evan Czaplicki <info@elm-lang.org>
2015-05-19 11:39:02 +0200

    Begin adding examples listing

--- src/pages/examples.elm
+++ src/pages/examples.elm
@@ -9,7 +9,16 @@ main =
   div []
     [ TopBar.topBar "examples"
     , Center.markdown "600px" content
+    , div [ Center.style "600px" ]
+        [ h1 [] [text "Examples"]
+        , ul [] (List.map viewSection core)
+        , ul [] (List.map viewSection visuals)
+        , ul [] (List.map viewSection signals)
+        ]
     ]
+
+
+(=>) = (,)
 
 
 content = """
@@ -25,10 +34,97 @@ you see new syntax or features!
 """
 
 
-function =
-  [ "apply"
-  , "forward-apply"
-  , "composition"
-  , "anonymous"
-  , "infix"
+-- VIEW EXAMPLES
+
+viewSection : Section -> Html
+viewSection (title, examples) =
+  li []
+    [ text title
+    , ul [] (List.map viewExample examples)
+    ]
+
+
+viewExample : (String, String) -> Html
+viewExample (name, url) =
+  li [] [ a [href url] [text name] ]
+
+
+-- EXAMPLES
+
+type alias Section = (String, List (String, String))
+
+
+visuals : List Section
+visuals =
+  [ "collage" =>
+      [ "lines" => "lines"
+      , "shapes" => "shapes"
+      , "text" => "text"
+      , "elements" => "elements"
+      , "transforms" => "transforms"
+      , "color" => "color"
+      , "linear gradient" => "linear-gradient"
+      , "radial gradient" => "radial-gradient"
+      , "texture" => "texture"
+      ]
+  ]
+
+
+core : List Section
+core =
+  [ "functions" =>
+      [ "apply" => "apply"
+      , "forward apply" => "forward-apply"
+      , "infixes" => "infix"
+      , "composition" => "composition"
+      , "anonymous" => "anonymous"
+      ]
+  , "recursion" =>
+      [ "factorial" => "factorial"
+      , "list length" => "length"
+      , "zip" => "zip"
+      , "quick sort" => "quick-sort"
+      ]
+  , "union types" =>
+      [ "maybe" => "maybe"
+      , "binary tree" => "binary-tree"
+      , "boolean expressions" => "boolean-expressions"
+      ]
+  , "containers" =>
+      [ "list" => "list"
+      , "dict" => "dict"
+      , "set" => "set"
+      ]
+  ]
+
+
+signals : List Section
+signals =
+  [ "mouse" =>
+      [ "position" => "position"
+      , "presses" => "isDown"
+      , "clicks" => "countClicks"
+      , "yogi" => "resizeYogi"
+      , "track" => "transforms"
+      ]
+  , "keyboard" =>
+      [ "arrows" => "arrows"
+      , "wasd" => "wasd"
+      , "keys Down" => "keysDown"
+      , "key Presses" => "charPressed"
+      ]
+  , "touch" =>
+      [ "raw" => "touches"
+      , "touches" => "touch"
+      , "taps" => "taps"
+      , "draw" => "draw"
+      ]
+  , "window" =>
+      [ "size" => "resizePaint"
+      , "centering" => "centering"
+      ]
+  , "time" =>
+      [ "fPS" => "fps"
+      , "every" => "every"
+      ]
   ]
