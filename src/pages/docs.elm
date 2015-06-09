f26ed794f50a6b10f4971853956e5714bc3ef492
Evan Czaplicki <info@elm-lang.org>
2015-06-08 17:38:44 -0700

    Add "Taxonomy of FRP" to advanced topics

--- src/pages/docs.elm
+++ src/pages/docs.elm
@@ -50,6 +50,10 @@ advancedStuff = """
     Gets into details of Daan Leijen's [paper][daan] that first described this
     design.
 
+  * [Taxonomy of FRP][taxonomy] &mdash; A talk that outlines the many flavors
+    of FRP. It describes how they work, how they relate to each other, and how
+    Elm ended up choosing a point in this design space.
+
   * [Concurrent FRP][conc] &mdash; Evan&rsquo;s senior thesis on Elm. Includes
     a very accessible history of FRP and overview of how signals work in Elm.
 
@@ -60,6 +64,7 @@ advancedStuff = """
 
 [ext]: /docs/records
 [daan]: http://research.microsoft.com/pubs/65409/scopedlabels.pdf
+[taxonomy]: https://www.youtube.com/watch?v=Agu6jipKfYw
 [conc]: /papers/concurrent-frp.pdf
 [async]: http://people.seas.harvard.edu/~chong/abstracts/CzaplickiC13.html
 
