a848f24c76f9f4df7c9c3c1c7337a4e84288541d
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2015-12-21 09:20:41 +0100

    replace /edit/examples/Elements/Markdown.elm link by /examples/markdown

--- src/pages/blog/announce/0.4.elm
+++ src/pages/blog/announce/0.4.elm
@@ -92,7 +92,7 @@ This blog post ([source][blog]) is actually written using this feature!
 I had no idea such a simple addition could have such a positive impact!
 
  [1]: http://daringfireball.net/projects/markdown/dingus "Markdown"
- [2]: /edit/examples/Elements/Markdown.elm "Elm+Markdown example"
+ [2]: /examples/markdown "Elm+Markdown example"
  [blog]: https://github.com/elm-lang/elm-lang.org/blob/master/src/pages/blog/announce/0.4.elm
 
 ## Games: Elements, Sprites, and Textures
