dfaecf1158672a73eaec8aef374d0fa3f49c68b8
Evan Czaplicki <evancz@users.noreply.github.com>
2016-07-25 13:56:19 -0700

    Update community page with some more recent info

--- src/pages/community.elm
+++ src/pages/community.elm
@@ -17,19 +17,18 @@ community = """
   * [Community Packages](http://package.elm-lang.org/)
   * [Mailing list][list]
   * [Reddit][reddit]
-  * [Slack][slack] and [IRC][irc]
+  * [Slack][slack]
   * [Twitter][twitter]
-  * [Real Life](http://www.meetup.com/Elm-user-group-SF/)
-  * [Potential projects](https://github.com/elm-lang/projects)
-  * [Contribution guidelines](https://github.com/elm-lang/elm-compiler/blob/master/CONTRIBUTING.md)
-
+  * [Real Life](#real-life)
+  * [Contributing](#Contribute)
+  
 <br>
 
 ## Mailing list
 
 [list]: https://groups.google.com/forum/?fromgroups#!forum/elm-discuss "mailing list"
 
-The mailing list [elm-discuss][list] is a great place for
+The [elm-discuss][list] mailing list is a great place for
 friendly discussion! Common activities include helping folks use Elm,
 discussing API design, reviewing blog posts and libraries, and finding projects
 to collaborate on.
@@ -39,6 +38,7 @@ about Elm or functional programming, be humble and open to learning new things
 from anyone! Try to read some old emails to get a feel for the culture and who
 everyone is.
 
+
 ## Reddit
 
 [reddit]: http://www.reddit.com/r/elm
@@ -46,14 +46,13 @@ everyone is.
 Check out [/r/elm][reddit] to find out about new libraries and read blog posts.
 Or even better, use it to announce libraries and post your own blog posts!
 
-## Slack and IRC
+
+## Slack
 
 [slack]: http://elmlang.herokuapp.com/
-[irc]: http://webchat.freenode.net/?channels=elm
 
 Got a quick question, but do not feel comfortable asking on the mailing list?
-Chatting on [Slack][slack] or [#elm][irc] is a great way to learn from a real
-person.
+Chatting on [Slack][slack] is a great way to quickly learn from a real person.
 
 As for culture, prefer to ask rather than tell. You may be talking to someone
 with no programming background or a PhD in programming languages, so to answer
@@ -72,33 +71,44 @@ Both [@elmlang][twitter] and [@czaplic](https://twitter.com/czaplic) tweet about
 Elm a lot. The #elmlang hashtag is commonly used. Using #elm is okay, but people
 tweet about weird stuff with that one sometimes.
 
+
 ## Real Life
 
-[Evan Czaplicki](http://github.com/evancz) will periodically organize [Elm user
-group](http://www.meetup.com/Elm-user-group-SF/) meetups in SF to learn and
-work on projects together. The format is great for supporting new users and
-encouraging interesting projects!
+There are meetups all over the world these days! The
+[elmweekly.nl](http://www.elmweekly.nl/) site tracks them all and posts info about
+when and where they will be.
 
-Think about organizing meetings in your city, it is fun!
+If you are thinking of starting a meetup in your city, check out how
+[Evan](https://github.com/evancz/) and [Richard](https://github.com/rtfeldman) organize
+the [Elm user group](http://www.meetup.com/Elm-user-group-SF/) in SF. A lot of existing
+meetups are structured based on [what we learned from experience][hack-night].
 
-There are a ton of Elm folks on the East coast, so Boston and New York would
-both be great places for meetups. The European community is also quite strong,
-London in particular it seems. We had [Elm Workshop][workshop] in 2013
-with speakers and attendees from all over Europe, so I think it is worth a
-shot!
-
-[workshop]: https://www.youtube.com/channel/UCzbnVYNyCwES9u3dqYZ-0WQ
+[hack-night]: http://tech.noredink.com/post/142283641812/designing-meetups-to-build-better-communities
 
 
 ## Contribute
 
-There are a bunch of projects in [the elm-lang
-organization](http://github.com/elm-lang), including the compiler, REPL, server,
-package manager, debugger, public library, and this website.
+**Every great contributor starts gradually.** At first that is means using Elm,
+maybe blogging about things they find interesting. After that, some folks
+will find things they want to make easier and build packages, like [elm-css][],
+that directly address their personal needs. This can be a great learning experience
+if you get folks in the community to review your API. It is also a great way to
+build relationships and trust with people. From there, people may work on projects
+like [these](https://github.com/elm-lang/projects) that are more core. This is
+how [elm-format][] and [elm-test][] were created.
 
-We have found that a good way to make contributions is to hang out on the
-[mailing list][list] to learn about the ongoing challenges. Becoming a part of
-this discussion will make it much clearer how you can effectively help the
-community or a specific project based on your skills and interests.
+[elm-css]: https://github.com/rtfeldman/elm-css
+[elm-format]: https://github.com/avh4/elm-format
+[elm-test]: https://github.com/elm-community/elm-test/pull/24
+
+Contribution usually means *technical* contribution, but community participation
+is often more valuable! Helping people with questions. Writing blog posts. Creating
+examples. This is all hugely positive for the community and the language, and I
+think engineer minds tend to underestimate its importance. Hanging out community
+forums also means you have more data on what people are struggling with, making it
+easier to find timely projects that matches your skills and interests.
+
+Point is, there are tons of ways to contribute! The goal should be to start gradually
+and figure out what works as you build a relationship with the community.
 
 """
