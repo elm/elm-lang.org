6966f7a60afdc2adf10f6da9000b6da1121edbbb
Evan Czaplicki <evancz@users.noreply.github.com>
2016-08-19 12:21:25 -0700

    Reorder /docs links, add /get-started

--- src/pages/docs.elm
+++ src/pages/docs.elm
@@ -20,28 +20,29 @@ content = """
 
 ### Quick Overview
 
+  * **[Get Started](/get-started)**
   * [Examples](/examples)
-  * [The Elm Architecture](http://guide.elm-lang.org/architecture/index.html)
+  * [The Elm Architecture](http://guide.elm-lang.org/architecture/)
   * [Let's be Mainstream!](http://www.elmbark.com/2016/03/16/mainstream-elm-user-focused-design)
 
 
-### References
+### Learn Elm
 
-  * **[The Guide](http://guide.elm-lang.org)**
+  * **[An Introduction to Elm](http://guide.elm-lang.org)**
+  * [FAQ](http://elm-community.github.io/elm-faq/)
   * [Syntax](/docs/syntax)
   * [Syntax vs JS](/docs/from-javascript)
   * [Style Guide](/docs/style-guide)
   * [Package Design](http://package.elm-lang.org/help/design-guidelines)
   * [Writing Documentation](http://package.elm-lang.org/help/documentation-format)
   * [Advanced Topics](/docs/advanced-topics)
-  * [FAQ for Learners](http://elm-community.github.io/elm-faq/)
 
 
 ### Packages
 
+  * **[All Community Packages](http://package.elm-lang.org)**
   * [Core](http://package.elm-lang.org/packages/elm-lang/core/latest/)
   * [HTML](http://package.elm-lang.org/packages/elm-lang/html/latest/)
-  * **[All Community Packages](http://package.elm-lang.org)**
 
 
 """
