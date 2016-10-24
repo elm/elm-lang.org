93cc604cfcccf1af8fa9f58e0a5295993bb1360d
Evan Czaplicki <evancz@users.noreply.github.com>
2016-10-24 14:01:27 -0700

    Fix the style of the 404 page

--- src/shared/Message.elm
+++ src/shared/Message.elm
@@ -1,10 +1,23 @@
 module Message exposing (report)
 
-import Html
+import Html exposing (..)
+import Html.Attributes exposing (..)
 import ColorScheme as C
 import Markdown
 
 
 report message =
-  Markdown.toHtml [] message
+  Markdown.toHtml [ reportStyle ] message
 
+
+(=>) = (,)
+
+
+reportStyle =
+  style
+    [ "width" => "300px"
+    , "margin" => "100px auto 0"
+    , "background" => "#F5F5F5"
+    , "padding" => "0 30px 10px"
+    , "border-top" => "4px solid #60B5CC"
+    ]
