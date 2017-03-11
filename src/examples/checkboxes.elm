89a674bc824afa9ab1a69025caf3b92248676fe6
mamachanko <sugardubz@gmail.com>
2017-03-11 09:21:27 +0100

    read checkbox state from model in view of checkbox example

--- src/examples/checkboxes.elm
+++ src/examples/checkboxes.elm
@@ -1,11 +1,12 @@
+module Main exposing (..)
+
 import Html exposing (Html, beginnerProgram, fieldset, input, label, text)
-import Html.Attributes exposing (style, type_)
+import Html.Attributes exposing (style, type_, checked)
 import Html.Events exposing (onClick)
 
 
-
 main =
-  beginnerProgram { model = optOut, update = update, view = view }
+    beginnerProgram { model = optOut, update = update, view = view }
 
 
 
@@ -13,15 +14,15 @@ main =
 
 
 type alias Model =
-  { notifications : Bool
-  , autoplay : Bool
-  , location : Bool
-  }
+    { notifications : Bool
+    , autoplay : Bool
+    , location : Bool
+    }
 
 
 optOut : Model
 optOut =
-  Model True True True
+    Model False False False
 
 
 
@@ -29,22 +30,22 @@ optOut =
 
 
 type Msg
-  = ToggleNotifications
-  | ToggleAutoplay
-  | ToggleLocation
+    = ToggleNotifications
+    | ToggleAutoplay
+    | ToggleLocation
 
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    ToggleNotifications ->
-      { model | notifications = not model.notifications }
+    case msg of
+        ToggleNotifications ->
+            { model | notifications = not model.notifications }
 
-    ToggleAutoplay ->
-      { model | autoplay = not model.autoplay }
+        ToggleAutoplay ->
+            { model | autoplay = not model.autoplay }
 
-    ToggleLocation ->
-      { model | location = not model.location }
+        ToggleLocation ->
+            { model | location = not model.location }
 
 
 
@@ -53,18 +54,23 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-  fieldset []
-    [ checkbox ToggleNotifications "Email Notifications"
-    , checkbox ToggleAutoplay "Video Autoplay"
-    , checkbox ToggleLocation "Use Location"
-    ]
+    fieldset []
+        [ checkbox ToggleNotifications "Email Notifications" model.notifications
+        , checkbox ToggleAutoplay "Video Autoplay" model.autoplay
+        , checkbox ToggleLocation "Use Location" model.location
+        ]
 
 
-checkbox : msg -> String -> Html msg
-checkbox msg name =
-  label
-    [ style [("padding", "20px")]
-    ]
-    [ input [ type_ "checkbox", onClick msg ] []
-    , text name
-    ]
+checkbox : msg -> String -> Bool -> Html msg
+checkbox msg name isChecked =
+    label
+        [ style [ ( "padding", "20px" ) ]
+        ]
+        [ input
+            [ type_ "checkbox"
+            , checked isChecked
+            , onClick msg
+            ]
+            []
+        , text name
+        ]
