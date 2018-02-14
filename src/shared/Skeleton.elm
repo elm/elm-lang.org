6ae471af172c3455b96e0d75f12626f173a98861
Louis Pilfold <louis@lpil.uk>
2018-02-14 18:09:42 +0000

    Change 2017 to 2018

--- src/shared/Skeleton.elm
+++ src/shared/Skeleton.elm
@@ -48,6 +48,6 @@ footer =
   div [class "footer"]
     [ text "All code for this site is open source and written in Elm. "
     , a [ class "grey-link", href "https://github.com/elm-lang/elm-lang.org/" ] [ text "Check it out" ]
-    , text "! — © 2012-2017 Evan Czaplicki"
+    , text "! — © 2012-2018 Evan Czaplicki"
     ]
 
