4a51f20e53779a4f567d94bf2b59ea3cd4b42274
Evan Czaplicki <evancz@users.noreply.github.com>
2018-04-17 21:06:48 +0200

    Make 404 page use fullscreen programs

--- src/pages/404.elm
+++ src/pages/404.elm
@@ -1,8 +1,34 @@
 
-import Message exposing (report)
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Markdown
 
 
-main = report """
+main : Program () () Never
+main =
+  Browser.fullscreen
+    { init = \_ -> ((), Cmd.none)
+    , update = \_ _ -> ((), Cmd.none)
+    , subscriptions = \_ -> Sub.none
+    , onNavigation = Nothing
+    , view = \_ ->
+        { title = "Page not found"
+        , body = [ notFound ]
+        }
+    }
+
+
+notFound : Html msg
+notFound =
+  Markdown.toHtml
+    [ style "width" "300px"
+    , style "margin" "100px auto 0"
+    , style "background" "#F5F5F5"
+    , style "padding" "0 30px 10px"
+    , style "border-top" "4px solid #60B5CC"
+    ]
+    """
 
 <h1><div>Poem 404
 <div style="font-size:0.5em;font-weight:normal">Page Not Found</div></div>
