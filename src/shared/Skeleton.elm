25f92a10d68be21a660738e8f73451b3482ac916
Evan Czaplicki <evancz@users.noreply.github.com>
2018-04-18 10:57:34 +0200

    Collapse Blog module into Skeleton module

--- src/shared/Skeleton.elm
+++ src/shared/Skeleton.elm
@@ -1,9 +1,52 @@
-module Skeleton exposing (skeleton)
+module Skeleton exposing
+  ( docs
+  , hints
+  , skeleton
+  , blog
+  , Author
+  , evan
+  , michael
+  , Date
+  )
 
 
 import Browser
+import Center
+import Dict
 import Html exposing (..)
 import Html.Attributes exposing (..)
+
+
+
+-- DOCS
+
+
+docs : String -> List (Html Never) -> Program () () Never
+docs title body =
+  skeleton title "docs"
+    [ div
+        [ style "padding" "4em 0 1em"
+        , style "text-align" "center"
+        ]
+        [ div [ style "font-size" "4em" ] [text title]
+        ]
+    , div [] body
+    ]
+
+
+
+-- HINTS
+
+
+hints : String -> String -> Program () () Never
+hints title markdown =
+  docs title
+    [ div
+        [ style "max-width" "600px"
+        , style "margin" "0 auto"
+        ]
+        [ Center.markdown "600px" markdown ]
+    ]
 
 
 
@@ -64,3 +107,89 @@ footer =
     , text "! — © 2012-2018 Evan Czaplicki"
     ]
 
+
+
+-- BLOG
+
+
+blog : String -> String -> Author -> Date -> List (Html Never) -> Program () () Never
+blog title subtitle author date body =
+  skeleton title "blog"
+    [ div
+        [ style "padding" "4em 0 1em"
+        , style "text-align" "center"
+        ]
+        [ div [ style "font-size" "4em" ] [text title]
+        , div [ style "font-size" "1.5em" ] [text subtitle]
+        , div [ class "author" ]
+            [ text "by "
+            , a [href author.url] [text author.name]
+            , text (" / " ++ dateToString date)
+            ]
+        ]
+    , div [] body
+    ]
+
+
+
+-- AUTHORS
+
+
+type alias Author =
+  { name : String
+  , url : String
+  }
+
+
+evan : Author
+evan =
+  { name = "Evan Czaplicki"
+  , url = "https://twitter.com/czaplic"
+  }
+
+
+michael : Author
+michael =
+  { name = "Michael James"
+  , url = "http://github.com/michaelbjames"
+  }
+
+
+
+-- DATES
+
+
+type alias Date =
+  { year : Int
+  , month : Int
+  , day : Int
+  }
+
+
+dateToString : Date -> String
+dateToString date =
+  case Dict.get date.month months of
+    Nothing ->
+      String.fromInt date.year
+
+    Just month ->
+      String.fromInt date.day ++ " " ++ month ++ " " ++ String.fromInt date.year
+
+
+
+months : Dict.Dict Int String
+months =
+  Dict.fromList
+    [ (1, "Jan")
+    , (2, "Feb")
+    , (3, "Mar")
+    , (4, "Apr")
+    , (5, "May")
+    , (6, "June")
+    , (7, "July")
+    , (8, "Aug")
+    , (9, "Sep")
+    , (10, "Oct")
+    , (11, "Nov")
+    , (12, "Dec")
+    ]
