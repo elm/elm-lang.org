10f3e37762516d7d71ead9cbcc16041bbec32a90
Evan Czaplicki <evancz@users.noreply.github.com>
2018-08-17 22:57:30 -0400

    Try again to get the symlink correct

--- build.sh
+++ build.sh
@@ -43,7 +43,7 @@ fi
 if [ ! -f bin/uglifyjs ]
 then
   npm install uglify-js
-  ln -s node_modules/uglify-js/bin/uglifyjs bin/uglifyjs
+  ln -s ../node_modules/uglify-js/bin/uglifyjs bin/uglifyjs
 fi
 PATH=$(pwd)/bin:$PATH
 
