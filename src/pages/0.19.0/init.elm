6b2182f9fb8d7c736be8b11e84ccfc31c3eef3bf
Evan Czaplicki <evancz@users.noreply.github.com>
2018-08-21 10:49:58 -0400

    Add a bit about testing

--- src/pages/0.19.0/init.elm
+++ src/pages/0.19.0/init.elm
@@ -23,4 +23,10 @@ So we recommend that newcomers staying in one file until you get into the 600 to
 The talk [The Life of a File](https://youtu.be/XpDsk374LDE) gets into this a lot more. The advice about building modules around a specific custom type is particularly important! You will see that emphasized a lot as you work through [the official guide](https://guide.elm-lang.org/).
 
 
+## Testing your Code
+
+Elm will catch a bunch of errors statically, but it also has a really great testing package called [`elm-explorations/test`](https://github.com/elm-explorations/test) that can help you catch the rest.
+
+This can be particularly helpful for teams working on a large codebase. When you are editing code you have never seen before, it can be helpful to capture additional details and constraints in tests!
+
 """
