55bcac99af9bd693263a7ee5a8b413da4058751d
Evan Czaplicki <evancz@users.noreply.github.com>
2018-08-22 11:45:06 -0400

    Redo the missing-patterns docs to match the guide better
    And give better advice in some sections.

--- src/pages/0.19.0/missing-patterns.elm
+++ src/pages/0.19.0/missing-patterns.elm
@@ -13,103 +13,84 @@ There are a couple techniques for making this work for you in every scenario.
 
 ## The danger of wildcard patterns
 
-A common scenario is that you want to add a tag to a union type that is used in a bunch of places. For example, maybe you are working with some widgets:
+A common scenario is that you want to add a tag to a custom type that is used in a bunch of places. For example, maybe you are working different variations of users in a chat room:
 
 ```elm
-type Widget
-    = ScatterPlot ...
-    | Table ...
+type User
+  = Regular String Int
+  | Anonymous
 
-
-isGraph : Widget -> Bool
-isGraph widget =
-  case widget of
-    ScatterPlot _ ->
-        True
+toName : User -> String
+toName user =
+  case user of
+    Regular name _ ->
+      name
 
     _ ->
-        False
+      "anonymous"
 ```
 
-Notice the wildcard pattern in `isGraph`. This will hurt us! When you add `BarChart` as a new kind of `Widget` you are introducing a bug that the compiler cannot catch. A `BarChart` is a graph, but the code deciding that is just lumping a bunch of stuff together.
+Notice the wildcard pattern in `toName`. This will hurt us! Say we add a `Visitor String` variant to `User` at some point. Now we have a bug that visitor names are reported as `"anonymous"`, and the compiler cannot help us!
 
-It is better to explicitly list all possible patterns, like this:
+So instead, it is better to explicitly list all possible variants, like this:
 
 ```elm
-isGraph : Widget -> Bool
-isGraph widget =
-  case widget of
-    ScatterPlot _ ->
-        True
+type User
+  = Regular String Int
+  | Visitor String
+  | Anonymous
 
-    Table _ ->
-        False
+toName : User -> String
+toName user =
+  case user of
+    Regular name _ ->
+      name
+
+    Anonymous ->
+      "anonymous"
 ```
 
-So now when you want to add `BarChart`, the compiler will say "hey, what should `isGraph` do when it sees a bar chart?" This is a tiny bit of extra work, but it is very worth it!
+Now the compiler will say "hey, what should `toName` do when it sees a `Visitor`?" This is a tiny bit of extra work, but it is very worth it!
 
 
 ## I want to go fast!
 
-Okay, so we are still editing that `Widget` code. We are not using any dangerous wildcards anymore, but there are 20 or 30 functions all pattern matching on `Widget` and doing something.
+Imagine that the `User` type appears in 20 or 30 functions across your project. When we add a `Visitor` variant, the compiler points out all the places that need to be updated. That is very convenient, but in a big project, maybe you want to get through it extra quickly.
+
+In that case, it can be helpful to use [`Debug.todo`](https://package.elm-lang.org/packages/elm-lang/core/latest/Debug#todo) to leave some code incomplete:
 
 ```elm
-type Widget
-    = ScatterPlot ...
-    | Table ...
+type User
+  = Regular String Int
+  | Visitor String
+  | Anonymous
 
+toName : User -> String
+toName user =
+  case user of
+    Regular name _ ->
+      name
 
-isGraph : Widget -> Bool
-isGraph widget =
-  case widget of
-    ScatterPlot _ ->
-        True
+    Visitor _ ->
+      Debug.todo "give the visitor name"
 
-    Table _ ->
-        False
-
+    Anonymous ->
+      "anonymous"
 
 -- and maybe a bunch of other things
 ```
 
-So now you add a new tag `BarChart` to `Widget`. This will trigger errors in all the functions that pattern match on widgets. In a big project, maybe this is a ton of functions.
+In this case it is easier to just write the implementation, but the point is that on more complex functions, you can put things off a bit.
 
-To get things running quickly, it can be helpful to use [`Debug.crash`](http://package.elm-lang.org/packages/elm-lang/core/latest/Debug#crash) temporarily. So you might update the code to this:
+The Elm compiler is actually aware of `Debug.todo` so when it sees it in a `case` like this, it will crash with a bunch of helpful information. It will tell you:
 
-```elm
-type Widget
-    = ScatterPlot ...
-    | Table ...
-    | BarChart ...
-
-
-isGraph : Widget -> Bool
-isGraph widget =
-  case widget of
-    ScatterPlot _ ->
-        True
-
-    Table _ ->
-        False
-
-    BarChart _ ->
-        Debug.crash "TODO"
-
-
--- and maybe a bunch of other things
-```
-
-Rather than figure out the exact details, just put in `Debug.crash "TODO"` for now. The Elm compiler is actually aware of `Debug.crash` so when it sees it in a `case` like this, it will augment the crash message with a bunch of helpful information. It will tell you:
-
-  1. The name of the module that contains the offending code.
-  2. The line numbers of the `case` containing the crash.
-  3. The particular value that led to this crashing branch.
+  1. The name of the module that contains the code.
+  2. The line numbers of the `case` containing the TODO.
+  3. The particular value that led to this TODO.
 
 From that information you have a pretty good idea of what went wrong and can go fix it.
 
-I tend to use `TODO` as the message when my goal is to go quick because it makes it easy to go and find all remaining todos in my code before a release.
-
-> And in 99.999% of cases, a `Debug.crash` should never make it to a release. It is nice for debugging (like it says in the name) but it is not really a good idea to use it in other ways. When you hit a `Debug.crash` the program really does crash. It is just done working.
+I tend to use `Debug.todo` as the message when my goal is to go quick because it makes it easy to go and find all remaining todos in my code before a release.
 
 
 ## A list that definitely is not empty
@@ -127,7 +108,7 @@ last list =
         last rest
 ```
 
-This is no good. It does not handle the empty list. The recommended definition would be:
+This is no good. It does not handle the empty list. There are two ways to handle this. One is to make the function return a `Maybe` like this:
 
 ```elm
 last : List a -> Maybe a
@@ -145,6 +126,21 @@ last list =
 
 This is nice because it lets users know that there might be a failure, so they can recover from it however they want.
 
-> An alternative would be to represent a non-empty list explicitly. For example, the type `(a, List a)` guarantees that there is at least one `a` value, so you could alternatively create a function `last : (a, List a) -> a` or `last : a -> List a -> a` to demand at least one item.
+The other option is to “unroll the list” one level to ensure that no one can ever provide an empty list in the first place:
 
-  """
+```elm
+last : a -> List a -> a
+last first rest =
+  case rest of
+    [] ->
+      first
+
+    newFirst :: newRest ->
+      last newFirst newRest
+```
+
+By demanding the first element of the list as an argument, it becomes impossible to call this function if you have an empty list!
+
+This “unroll the list” trick is quite useful. I recommend using it directly, not through some external library. It is nothing special. Just a useful idea!
+
+"""
