2d120267fb5397739c67db7de85cd8c25b4dccce
Evan Czaplicki <evancz@users.noreply.github.com>
2018-11-07 14:25:38 -0500

    Explain what is going on with the examples for now

--- src/pages/examples.elm
+++ src/pages/examples.elm
@@ -11,13 +11,8 @@ main =
     "Elm - Examples"
     "examples"
     [ Center.markdown "600px" content
-    , div
-        (style "padding" "0 0.5em" :: Center.styles "600px")
-        [ view "HTML" html
-        , view "Core Language" fundamentals
-        , view "Effects" effects
-        , view "Functional Thinking" core
-        ]
+    , div [ class "buttons", style "text-align" "center" ]
+        [ a [ href "https://guide.elm-lang.org/" ] [ text "Official Guide" ]
     ]
 
 
@@ -25,119 +20,24 @@ content = """
 
 # Examples
 
-Playing with small examples is a great way to get comfortable with Elm! Think of them as a supplement to resources like the [syntax reference](/docs/syntax) and [guide.elm-lang.org](https://guide.elm-lang.org/).
+Elm was originally released with a simple online editor and a small set of
+examples. Not all of the examples match nicely with the modern learning resources
+about Elm, so it felt like it would be better to point folks to resources like
+[The Official Guide](https://guide.elm-lang.org/) for now.
+
+More specifically, the initial vision for the online editor and examples was to
+make it really easy for beginners to start playing around. It had simple 2D
+games and 3D animations. It was really neat, but over time, the learning resources
+become more focused on folks trying to make web apps. Many examples did not really
+fit with that, making the examples feel out of place for the audience coming from
+JavaScript.
+
+Both of these audiences are important though! So rather than just keeping it
+sort of confusing for everyone, I think it makes sense to start improving the
+learning resources for beginners. In the end, someone new to programming and
+someone coming from JavaScript are going to be interested in very different
+examples, so focusing on different audiences directly seems like the best path
+to figuring out how an examples page should work. Especially when there are all
+sorts of different people checking out the language these days.
 
 """
-
-
-
--- VIEW EXAMPLES
-
-
-view : String -> List Section -> Html msg
-view title sections =
-  div
-    [ class "examples"
-    , style "width" "300px"
-    , style "display" "inline-block"
-    , style "vertical-align" "top"
-    ]
-    [ h3 [] [text title]
-    , ul [] (List.map viewSection sections)
-    ]
-
-
-viewSection : Section -> Html msg
-viewSection (title, examples) =
-  li []
-    [ text title
-    , ul [] (List.map viewExample examples)
-    ]
-
-
-viewExample : (String, String) -> Html msg
-viewExample (name, fileName) =
-  let
-    url =
-      "/examples/" ++ fileName
-  in
-    li [] [ a [href url] [text name] ]
-
-
-
--- EXAMPLES
-
-
-type alias Section = (String, List (String, String))
-
-
-fundamentals : List Section
-fundamentals =
-  [ ( "Primitives"
-    , [ ("math", "math")
-      , ("strings", "strings")
-      , ("calling functions", "functions")
-      , ("defining functions", "define-functions")
-      ]
-    )
-  , ( "Syntax"
-    , [ ("if", "if")
-      , ("let", "let")
-      , ("case", "case")
-      , ("lambda", "lambda")
-      , ("pipes", "pipes")
-      , ("types", "types")
-      ]
-    )
-  ]
-
-
-core : List Section
-core =
-  [ ( "Recursion"
-    , [ ("list length", "length")
-      , ("zip", "zip")
-      , ("quick sort", "quick-sort")
-      , ("merge sort", "merge-sort")
-      ]
-    )
-  , ( "Custom Types"
-    , [ ("binary tree", "binary-tree")
-      , ("boolean expressions", "boolean-expressions")
-      ]
-    )
-  ]
-
-
-html : List Section
-html =
-  [ ( "Basics"
-    , [ ("hello world!", "hello-html")
-      , ("unordered list", "unordered-list")
-      , ("markdown", "markdown")
-      ]
-    )
-  , ( "User Input"
-    , [ ("buttons", "buttons")
-      , ("field", "field")
-      , ("form", "form")
-      , ("checkboxes", "checkboxes")
-      , ("radio buttons", "radio-buttons")
-      ]
-    )
-  ]
-
-
-effects : List Section
-effects =
-  [ ( "Commands"
-    , [ ("random", "random")
-      , ("http", "http")
-      ]
-    )
-  , ( "Subscriptions"
-    , [ ("time", "time")
-      , ("mouse drags", "drag")
-      ]
-    )
-  ]
