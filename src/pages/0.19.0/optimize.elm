6390c21833306e62551066f595adcf62985fdc7d
Kacper Jankowski <kacper@automotivemastermind.com>
2019-02-27 12:07:20 +0100

    Fixed command.

--- src/pages/0.19.0/optimize.elm
+++ src/pages/0.19.0/optimize.elm
@@ -49,7 +49,7 @@ min="elm.min.js"
 
 elm make --optimize --output=$js $@
 
-uglifyjs $js --compress pure_funcs=['F2','F3','F4','F5','F6','F7','F8','F9','A2','A3','A4','A5','A6','A7','A8','A9'],pure_getters,keep_fargs=false,unsafe_comps,unsafe,pure_getters,keep_fargs=false,unsafe_comps,unsafe' | uglifyjs --mangle --output=$min
+uglifyjs $js --compress pure_funcs=['F2','F3','F4','F5','F6','F7','F8','F9','A2','A3','A4','A5','A6','A7','A8','A9'],pure_getters,keep_fargs=false,unsafe_comps,unsafe,pure_getters,keep_fargs=false,unsafe_comps,unsafe | uglifyjs --mangle --output=$min
 
 echo "Initial size: $(cat $js | wc -c) bytes  ($js)"
 echo "Minified size:$(cat $min | wc -c) bytes  ($min)"
