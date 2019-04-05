4c53f838e8c123822f7946f6cb2da8aee7b53bc0
Evan Czaplicki <evancz@users.noreply.github.com>
2019-04-05 16:48:54 -0400

    Add a couple file examples

---
+++ examples/drag-and-drop.elm
@@ -0,0 +1,128 @@
+-- elm install elm/file
+-- elm install elm/json
+
+
+import Browser
+import File exposing (File)
+import File.Select as Select
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Json.Decode as D
+
+
+
+-- MAIN
+
+
+main =
+  Browser.element
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+
+
+-- MODEL
+
+
+type alias Model =
+  { hover : Bool
+  , files : List File
+  }
+
+
+init : () -> (Model, Cmd Msg)
+init _ =
+  (Model False [], Cmd.none)
+
+
+
+-- UPDATE
+
+
+type Msg
+  = Pick
+  | DragEnter
+  | DragLeave
+  | GotFiles File (List File)
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Pick ->
+      ( model
+      , Select.files ["image/*"] GotFiles
+      )
+
+    DragEnter ->
+      ( { model | hover = True }
+      , Cmd.none
+      )
+
+    DragLeave ->
+      ( { model | hover = False }
+      , Cmd.none
+      )
+
+    GotFiles file files ->
+      ( { model
+            | files = file :: files
+            , hover = False
+        }
+      , Cmd.none
+      )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  div
+    [ style "border" (if model.hover then "6px dashed purple" else "6px dashed #ccc")
+    , style "border-radius" "20px"
+    , style "width" "480px"
+    , style "height" "100px"
+    , style "margin" "100px auto"
+    , style "padding" "20px"
+    , style "display" "flex"
+    , style "flex-direction" "column"
+    , style "justify-content" "center"
+    , style "align-items" "center"
+    , hijackOn "dragenter" (D.succeed DragEnter)
+    , hijackOn "dragover" (D.succeed DragEnter)
+    , hijackOn "dragleave" (D.succeed DragLeave)
+    , hijackOn "drop" dropDecoder
+    ]
+    [ button [ onClick Pick ] [ text "Upload Images" ]
+    , span [ style "color" "#ccc" ] [ text (Debug.toString model) ]
+    ]
+
+
+dropDecoder : D.Decoder Msg
+dropDecoder =
+  D.at ["dataTransfer","files"] (D.oneOrMore GotFiles File.decoder)
+
+
+hijackOn : String -> D.Decoder msg -> Attribute msg
+hijackOn event decoder =
+  preventDefaultOn event (D.map hijack decoder)
+
+
+hijack : msg -> (msg, Bool)
+hijack msg =
+  (msg, True)
---
+++ examples/upload.elm
@@ -0,0 +1,82 @@
+-- elm install elm/file
+-- elm install elm/json
+
+
+import Browser
+import File exposing (File)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Json.Decode as D
+
+
+
+-- MAIN
+
+
+main =
+  Browser.element
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+
+
+-- MODEL
+
+
+type alias Model = List File
+
+
+init : () -> (Model, Cmd Msg)
+init _ =
+  ([], Cmd.none)
+
+
+
+-- UPDATE
+
+
+type Msg
+  = GotFiles (List File)
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    GotFiles files ->
+      (files, Cmd.none)
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ input
+        [ type_ "file"
+        , multiple True
+        , on "change" (D.map GotFiles filesDecoder)
+        ]
+        []
+    , div [] [ text (Debug.toString model) ]
+    ]
+
+
+filesDecoder : D.Decoder (List File)
+filesDecoder =
+  D.at ["target","files"] (D.list File.decoder)
+
