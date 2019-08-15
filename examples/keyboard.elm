987a87fe788f9ab81909800a933f1e4e4fbbe058
Evan Czaplicki <evancz@users.noreply.github.com>
2019-08-15 12:33:02 -0400

    add comment on keyboard example

--- examples/keyboard.elm
+++ examples/keyboard.elm
@@ -1,13 +1,22 @@
+-- Move a square around with the arrow keys: UP, DOWN, LEFT, RIGHT
+-- Try making it move around more quickly!
+--
+-- Learn more about the playground here:
+--   https://package.elm-lang.org/packages/evancz/elm-playground/latest/
+--
+
 import Playground exposing (..)
 
 
 main =
   game view update (0,0)
 
+
 view computer (x,y) =
   [ square blue 40
       |> move x y
   ]
+
 
 update computer (x,y) =
   ( x + toX computer.keyboard
