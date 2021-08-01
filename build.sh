212543b3a981e27bdc9136d850494bd9f079c21c
Tereza Sokol <terezasokol@gmail.com>
2021-08-01 15:48:03 +0200

    Add additional icon

--- build.sh
+++ build.sh
@@ -71,6 +71,10 @@ function makeExampleHtml {
     <polyline points="23 20 23 14 17 14"></polyline>
     <path d="M20.49 9A9 9 0 0 0 5.64 5.64L1 10m22 4l-4.64 4.36A9 9 0 0 1 3.51 15"></path>
   </symbol>
+  </symbol id="x" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+    <line x1="18" y1="6" x2="6" y2="18"></line>
+    <line x1="6" y1="6" x2="18" y2="18"></line>
+  </symbol>
 </svg>
 
 <!-- NAVIGATION -->
