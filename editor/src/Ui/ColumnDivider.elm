a2e742090438476158f212ad814f7993e5c457bb
Tereza Sokol <terezasokol@gmail.com>
2021-08-17 18:00:50 +0200

    Clamp in view, not update

--- editor/src/Ui/ColumnDivider.elm
+++ editor/src/Ui/ColumnDivider.elm
@@ -50,13 +50,13 @@ halfPoint =
 
 clamp : Window -> Float -> Float
 clamp window =
-  toPercentage window >> Basics.max (lowerLimit window) >> Basics.min (upperLimit window)
+  Basics.max (lowerLimit window) >> Basics.min (upperLimit window)
 
 
 jump : Window -> Float -> Float
 jump window percent =
   if percent <= lowerLimit window then upperLimit window
-  else if percent >= upperLimit window then halfPoint
+  else if percent >= upperLimit window then (if window.width <= 1000 then lowerLimit window else halfPoint)
   else if percent > halfPoint then upperLimit window
   else lowerLimit window
 
@@ -112,19 +112,19 @@ update window msg model =
       case model.movement of
         Moving initial False ->
           if isSignificant initial latest
-          then { model | percent = clamp window latest, movement = Moving initial True }
-          else { model | percent = clamp window latest }
+          then { model | percent = toPercentage window latest, movement = Moving initial True }
+          else { model | percent = toPercentage window latest }
 
         Moving _ True ->
-          { model | percent = clamp window latest }
+          { model | percent = toPercentage window latest }
 
         None ->
-          { model | percent = clamp window latest }
+          { model | percent = toPercentage window latest }
 
     OnUp latest ->
       case model.movement of
         Moving _ True ->
-          { model | movement = None, percent = clamp window latest }
+          { model | movement = None, percent = toPercentage window latest }
 
         Moving _ False ->
           { model | movement = None, percent = jump window model.percent }
@@ -144,50 +144,53 @@ update window msg model =
 
 view : (Msg -> msg) -> Window -> Model -> List (Html msg) -> List (Html msg) -> Html msg
 view onMsg window model leftChildren rightChildren =
+  let percent =
+        clamp window model.percent
+  in
   div
     [ id "double-pane"
     , style "width" "100%"
     , style "display" "flex"
     ]
-    [ viewLeft onMsg window model leftChildren
-    , Html.map onMsg (viewDivider window model)
-    , viewRight window model rightChildren
+    [ viewLeft onMsg window model percent leftChildren
+    , Html.map onMsg (viewDivider window model percent)
+    , viewRight window model percent rightChildren
     ]
 
 
-viewLeft : (Msg -> msg) -> Window -> Model -> List (Html msg) -> Html msg
-viewLeft onMsg window model =
+viewLeft : (Msg -> msg) -> Window -> Model -> Float -> List (Html msg) -> Html msg
+viewLeft onMsg window model percent =
   div
     [ id "left-side"
     , onClick (onMsg OnClickLeft)
-    , style "width" (String.fromFloat model.percent ++ "%")
+    , style "width" (String.fromFloat percent ++ "%")
     , style "pointer-events" (if isMoving model then "none" else "auto")
     , style "user-select" (if isMoving model then "none" else "auto")
     , style "transition" (if isMoving model then "none" else "width 0.5s")
     ]
 
 
-viewRight : Window -> Model -> List (Html msg) -> Html msg
-viewRight window model =
+viewRight : Window -> Model -> Float -> List (Html msg) -> Html msg
+viewRight window model percent =
   div
     [ id "right-side"
-    , style "width" (String.fromFloat (100 - model.percent) ++ "%")
+    , style "width" (String.fromFloat (100 - percent) ++ "%")
     , style "pointer-events" (if isMoving model then "none" else "auto")
     , style "user-select" (if isMoving model then "none" else "auto")
     , style "transition" (if isMoving model then "none" else "width 0.5s")
     ]
 
 
-viewDivider : Window -> Model -> Html Msg
-viewDivider window model =
+viewDivider : Window -> Model -> Float -> Html Msg
+viewDivider window model percent =
   node "column-divider"
     [ on "down" (D.map OnDown decodePixels)
     , on "move" (D.map OnMove decodePixels)
     , on "up" (D.map OnUp decodePixels)
     , on "_click" (D.succeed OnClick)
-    , property "pixels" (E.float (fromPercentage window model.percent))
+    , property "pixels" (E.float (fromPercentage window percent))
     , style "width" (if isUpperLimit window model then "40px" else "10px")
-    , style "left" (String.fromFloat model.percent ++ "%")
+    , style "left" (String.fromFloat percent ++ "%")
     ]
     []
 
