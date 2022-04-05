cfba1018826ae27085c4fe1dc60fbbb39f20d042
Evan Czaplicki <evancz@users.noreply.github.com>
2022-04-05 03:44:56 +0200

    attempt to add /example/quotes example

---
+++ examples/quotes.elm
@@ -0,0 +1,136 @@
+-- Press a button to send a GET request for random quotes.
+--
+-- Read how it works:
+--   https://guide.elm-lang.org/effects/json.html
+--
+
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http
+import Json.Decode as D exposing (Decoder, field, int, string)
+
+
+
+-- MAIN
+
+
+main =
+  Browser.element
+    { init = init
+    , update = update
+    , subscriptions = subscriptions
+    , view = view
+    }
+
+
+
+-- MODEL
+
+
+type Model
+  = Failure
+  | Loading
+  | Success Quote
+
+
+type alias Quote =
+  { title : String
+  , author : String
+  , year : Int
+  , text : String
+  }
+
+
+init : () -> (Model, Cmd Msg)
+init _ =
+  (Loading, getRandomQuote)
+
+
+
+-- UPDATE
+
+
+type Msg
+  = MorePlease
+  | GotQuote (Result Http.Error Quote)
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    MorePlease ->
+      (Loading, getRandomQuote)
+
+    GotQuote result ->
+      case result of
+        Ok quote ->
+          (Success quote, Cmd.none)
+
+        Err _ ->
+          (Failure, Cmd.none)
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ h2 [] [ text "Random Quotes" ]
+    , viewGif model
+    ]
+
+
+viewGif : Model -> Html Msg
+viewGif model =
+  case model of
+    Failure ->
+      div []
+        [ text "I could not load a random quote for some reason. "
+        , button [ onClick MorePlease ] [ text "Try Again!" ]
+        ]
+
+    Loading ->
+      text "Loading..."
+
+    Success quote ->
+      div []
+        [ button [ onClick MorePlease, style "display" "block" ] [ text "More Please!" ]
+        , h3 [] [ text quote.title ]
+        , p [] [ text (quote.author ++ " (" ++ String.fromInt quote.year ++ ")") ]
+        , p [] [ text quote.text ]
+        ]
+
+
+
+-- HTTP
+
+
+getRandomQuote : Cmd Msg
+getRandomQuote =
+  Http.get
+    { url = "https://elm-lang.org/api/random-quotes"
+    , expect = Http.expectJson GotQuote quoteDecoder
+    }
+
+
+quoteDecoder : Decoder String
+quoteDecoder =
+  D.map4 Quote
+    (field "title" string)
+    (field "author" string)
+    (field "year" int)
+    (field "text" string)
+
